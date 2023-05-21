4a4601a71792c2a8358221874e07a264d2dfe175
hamiiid <Hamiiid.Asgarian@gmail.com>
2023-05-21 15:21:00 +0330

    reorganize the services instance

--- lib/blocs/services_bloc.dart
+++ lib/blocs/services_bloc.dart
@@ -1,5 +1,7 @@
 /// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_skeleton/services/localization_service.dart';
+import 'package:flutter_skeleton/services/sounds_service.dart';
 
 import '../../services/network_service.dart';
 import '../services/ads_service.dart';
@@ -12,60 +14,76 @@ class ServicesEvent {}
 
 abstract class ServicesState {
   final IAdsService adsService;
-  final GameApisService gameApiData;
-  final AnalyticsService analyticsData;
-  final NetworkService netConnectionData;
+  final GameApisService gameApiService;
+  final AnalyticsService analyticsService;
+  final NetworkService networkService;
+  final LocalizationService localizationService;
+  final SoundService soundService;
 
-  ServicesState(
-      {required this.adsService,
-      required this.gameApiData,
-      required this.analyticsData,
-      required this.netConnectionData});
+  ServicesState({
+    required this.gameApiService,
+    required this.analyticsService,
+    required this.networkService,
+    required this.localizationService,
+    required this.soundService,
+    required this.adsService,
+  });
 }
 
 class ServicesInit extends ServicesState {
-  ServicesInit({
-    required super.adsService,
-    required super.gameApiData,
-    required super.analyticsData,
-    required super.netConnectionData,
-  });
+  ServicesInit(
+      {required super.gameApiService,
+      required super.analyticsService,
+      required super.networkService,
+      required super.localizationService,
+      required super.soundService,
+      required super.adsService});
 }
 
 class ServicesUpdate extends ServicesState {
   ServicesUpdate(
-      {required super.adsService,
-      required super.gameApiData,
-      required super.analyticsData,
-      required super.netConnectionData});
+      {required super.gameApiService,
+      required super.analyticsService,
+      required super.networkService,
+      required super.localizationService,
+      required super.soundService,
+      required super.adsService});
 }
 
 //--------------------------------------------------------
 
 class ServicesBloc extends Bloc<ServicesEvent, ServicesState> {
   IAdsService adsService;
-  GameApisService gameApiData;
-  AnalyticsService analyticsData;
-  NetworkService netConnectionData;
+  GameApisService gameApiService;
+  AnalyticsService analyticsService;
+  NetworkService networkService;
+  LocalizationService localizationService;
+  SoundService soundService;
 
   ServicesBloc(
       {required this.adsService,
-      required this.gameApiData,
-      required this.analyticsData,
-      required this.netConnectionData})
+      required this.gameApiService,
+      required this.analyticsService,
+      required this.networkService,
+      required this.localizationService,
+      required this.soundService})
       : super(ServicesInit(
             adsService: adsService,
-            gameApiData: gameApiData,
-            analyticsData: analyticsData,
-            netConnectionData: netConnectionData)) {
+            analyticsService: analyticsService,
+            gameApiService: gameApiService,
+            localizationService: localizationService,
+            networkService: networkService,
+            soundService: soundService)) {
     // on<ServicesEvent>(update);
   }
 
-  initialize() {
-    adsService.log("adsService");
-    analyticsData.log("analyticsService");
-    netConnectionData.log("netConnectionService");
-    gameApiData.log("gameApiService");
+  initialize() async {
+    await adsService.initialize();
+    await analyticsService.initialize();
+    await networkService.initialize();
+    await gameApiService.initialize();
+    await localizationService.initialize();
+    await soundService.initialize();
   }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_skeleton/services/localization_service.dart';
 import 'package:flutter_skeleton/services/sounds_service.dart';
 // import 'package:fc_project/viewModel/bloc/services_bloc.dart';
 
@@ -15,44 +16,54 @@ import 'view/pages/loading_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  NetworkService network = Network();
-  SoundService sound = Sounds();
-  GameApisService gameApi = MainGameApi();
-  AnalyticsService analytics = Analytics();
-  IAdsService ads = AdsService(analytics: analytics, sound: sound);
+  // NetworkService network = Network();
+  // SoundService sound = Sounds();
+  // GameApisService gameApi = MainGameApi();
+  // AnalyticsService analytics = Analytics();
+  // IAdsService ads = AdsService(analytics: analytics, sound: sound);
 
   runApp(MyApp(
-    netConnectionData: network,
-    gameApiData: gameApi,
-    adsData: ads,
-    analyticsData: analytics,
-  ));
+      // netConnectionData: network,
+      // gameApiData: gameApi,
+      // adsData: ads,
+      // analyticsData: analytics,
+      ));
 }
 
 class MyApp extends StatelessWidget {
-  final IAdsService adsData;
-  final GameApisService gameApiData;
-  final NetworkService netConnectionData;
-  final AnalyticsService analyticsData;
+  // final IAdsService adsData;
+  // final GameApisService gameApiData;
+  // final NetworkService netConnectionData;
+  // final AnalyticsService analyticsData;
 
   const MyApp({
     super.key,
-    required this.adsData,
-    required this.gameApiData,
-    required this.analyticsData,
-    required this.netConnectionData,
+    // required this.adsData,
+    // required this.gameApiData,
+    // required this.analyticsData,
+    // required this.netConnectionData,
   });
 
   @override
   Widget build(BuildContext context) {
+    NetworkService netConnection = Network();
+    SoundService sound = Sounds();
+    GameApisService gameApi = MainGameApi();
+    AnalyticsService analytics = Analytics();
+    IAdsService adsData = AdsService(analytics: analytics, sound: sound);
+    LocalizationService localization = ILocalization();
+    SoundService sounds = Sounds();
+
     return MultiBlocProvider(
       providers: [
         BlocProvider(
           create: (context) => ServicesBloc(
               adsService: adsData,
-              gameApiData: gameApiData,
-              analyticsData: analyticsData,
-              netConnectionData: netConnectionData),
+              gameApiService: gameApi,
+              analyticsService: analytics,
+              networkService: netConnection,
+              localizationService: localization,
+              soundService: sounds),
         ),
       ],
       child: const MaterialApp(home: LoadingScreen()),
