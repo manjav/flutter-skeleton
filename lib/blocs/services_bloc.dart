63e2c052d14c79d38776c21f996bb817f1dcb019
Hamiiid <hamiiid.asgarian@gmail.com>
2023-06-10 15:36:36 +0330

    Revert "Add `IntegratedServices`"
    This reverts commit 6fd07aba249e581ecb45310cd5a50b269d64d18b.

--- lib/blocs/services_bloc.dart
+++ lib/blocs/services_bloc.dart
@@ -1,35 +1,108 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../services/core/integrated_services.dart';
+
+import '../services/core/ads/ads_service.dart';
+import '../services/game_service.dart';
+import '../services/localization.dart';
+import '../services/network.dart';
+import '../services/prefs.dart';
+import '../services/sounds.dart';
+import '../services/theme.dart';
+import '../services/trackers/trackers_service.dart';
 
 class ServicesEvent {}
 
 //--------------------------------------------------------
 
 abstract class ServicesState {
-  final IntegratedServices integratedServices;
+  final AdsService adsService;
+  final IGameService gameApiService;
+  final TrackersService analyticsService;
+  final Localization localizationService;
+  final INetwork networkService;
+  final Prefs prefsService;
+  final ISound soundService;
+  final ITheme themeService;
 
-  ServicesState({required this.integratedServices});
+  ServicesState({
+    required this.prefsService,
+    required this.gameApiService,
+    required this.adsService,
+    required this.analyticsService,
+    required this.networkService,
+    required this.localizationService,
+    required this.soundService,
+    required this.themeService,
+  });
 }
 
 class ServicesInit extends ServicesState {
-  ServicesInit({required super.integratedServices});
+  ServicesInit({
+    required super.prefsService,
+    required super.gameApiService,
+    required super.analyticsService,
+    required super.networkService,
+    required super.localizationService,
+    required super.soundService,
+    required super.adsService,
+    required super.themeService,
+  });
 }
 
 class ServicesUpdate extends ServicesState {
-  ServicesUpdate({required super.integratedServices});
+  ServicesUpdate({
+    required super.prefsService,
+    required super.gameApiService,
+    required super.analyticsService,
+    required super.networkService,
+    required super.localizationService,
+    required super.soundService,
+    required super.adsService,
+    required super.themeService,
+  });
 }
 
 //--------------------------------------------------------
 
 class ServicesBloc extends Bloc<ServicesEvent, ServicesState> {
-  IntegratedServices integratedServices;
+  AdsService adsService;
+  IGameService gameApiService;
+  TrackersService trackers;
+  Localization localization;
+  INetwork network;
+  ISound sound;
+  ITheme theme;
+  Prefs prefs;
 
-  ServicesBloc({required this.integratedServices})
-      : super(ServicesInit(integratedServices: integratedServices)) {
+  ServicesBloc({
+    required this.adsService,
+    required this.gameApiService,
+    required this.trackers,
+    required this.localization,
+    required this.network,
+    required this.prefs,
+    required this.sound,
+    required this.theme,
+  }) : super(ServicesInit(
+          adsService: adsService,
+          analyticsService: trackers,
+          gameApiService: gameApiService,
+          localizationService: localization,
+          networkService: network,
+          prefsService: prefs,
+          soundService: sound,
+          themeService: theme,
+        )) {
     // on<ServicesEvent>(update);
   }
 
   initialize() async {
-    integratedServices.init();
+    await prefs.initialize();
+    await localization.initialize();
+    await theme.initialize();
+    await sound.initialize();
+    await gameApiService.initialize();
+    await network.initialize();
+    await trackers.initialize();
+    await adsService.initialize();
   }
 }
