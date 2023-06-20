67a2d02b9094dd8b4dc77529eca6e8a350e6e4f1
Mansour <mansurjavadi@gmail.com>
2023-06-20 14:51:45 +0300

    Handle server errors

--- lib/view/overlays/loading_overlay.dart
+++ lib/view/overlays/loading_overlay.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rive/rive.dart';
 
-import '../../blocs/account_bloc.dart';
-import '../../data/core/account.dart';
+import '../../blocs/services.dart';
+import '../../services/deviceinfo.dart';
+import '../../services/iservices.dart';
+import '../../services/localization.dart';
+import '../../services/theme.dart';
 import '../../utils/utils.dart';
 import '../../view/screens/iscreen.dart';
+import '../widgets.dart';
 import 'ioverlay.dart';
 
 class LoadingOverlay extends AbstractOverlay {
@@ -16,9 +20,17 @@ class LoadingOverlay extends AbstractOverlay {
 }
 
 class _LoadingOverlayState extends AbstractOverlayState<AbstractOverlay> {
+  bool _logViewVisibility = false;
   Widget _alert = const SizedBox();
   SMIBool? _closeInput;
-  var logViewVisibility = false;
+  int _startTime = 0;
+  final _minAnimationTime = 3000;
+
+  @override
+  void initState() {
+    _startTime = DateTime.now().millisecondsSinceEpoch;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,9 +41,7 @@ class _LoadingOverlayState extends AbstractOverlayState<AbstractOverlay> {
           artboard,
           'Loading',
           onStateChange: (state, animation) {
-            if (animation == "closed") {
-              close();
-            }
+            if (animation == "closed") close();
           },
         );
         _closeInput = controller!.findInput<bool>('close') as SMIBool;
@@ -43,65 +53,62 @@ class _LoadingOverlayState extends AbstractOverlayState<AbstractOverlay> {
           if (state.initState == ServicesInitState.complete) {
             _closeInput?.value = true;
           } else if (state.initState == ServicesInitState.initialize) {
-            Navigator.pushNamed(context, Screens.home.routeName);
+            // wait for minimum animation time
+            var elapsedTime =
+                DateTime.now().millisecondsSinceEpoch - _startTime;
+            await Future.delayed(
+                Duration(milliseconds: _minAnimationTime - elapsedTime));
+            if (mounted) {
+              Navigator.pushNamed(context, Screens.home.routeName);
+            }
+          } else if (state.initState == ServicesInitState.error) {
+            _alert = Positioned(
+                bottom: 48.d,
+                child: Column(
+                  children: [
+                    Text(
+                      "Connection Lost!\nPlease try again.",
+                      textAlign: TextAlign.center,
+                      style: TStyles.medium,
+                    ),
+                    SizedBox(height: 16.d),
+                    Widgets.button(
+                        buttonId: -1,
+                        child: Text('Retry', style: TStyles.large),
+                        width: 180.d,
+                        color: TColors.blue,
+                        onPressed: () {
+                          _reload();
+                        })
+                  ],
+                ));
+            setState(() {});
           }
         },
       ),
-      // Positioned(
-      //     bottom: 4.d,
-      //     right: 16.d,
-      //     child: _services.localization.isInitialized
-      //         ? Text("v.${'app_version'.l()}", style: TStyles.smallInvert)
-      //         : const SizedBox()),
-      // Positioned(
-      //     bottom: 4.d,
-      //     left: 16.d,
-      //     child: Text(Device.adId, style: TStyles.tinyInvert)),
-      // Positioned(
-      //     top: 4.d,
-      //     right: 4.d,
-      //     bottom: 4.d,
-      //     left: 4.d,
-      //     child: GestureDetector(
-      //         onDoubleTap: () {
-      //           setState(() => logViewVisibility = !logViewVisibility);
-      //         },
-      //         child: logViewVisibility
-      //             ? Text(_services.connection.accumulatedLog,
-      //                 style: TStyles.tinyInvert)
-      //             : Widgets.rect(color: TColors.transparent))),
+      Positioned(
+          bottom: 4.d,
+          right: 16.d,
+          child: Text("v.${'app_version'.l()}", style: TStyles.smallInvert)),
+      Positioned(
+          bottom: 4.d,
+          left: 16.d,
+          child: Text(DeviceInfo.adId, style: TStyles.tinyInvert)),
+      Positioned(
+          top: 4.d,
+          right: 4.d,
+          bottom: 4.d,
+          left: 4.d,
+          child: GestureDetector(
+              onDoubleTap: () {
+                setState(() => _logViewVisibility = !_logViewVisibility);
+              },
+              child: _logViewVisibility
+                  ? Text(IService.accumulatedLog, style: TStyles.tinyInvert)
+                  : Widgets.rect(color: TColors.transparent))),
       _alert,
     ]);
   }
-
-  /* _onNetworkEventChange() {
-    setState(() {});
-    if (_services.connection.response.state == LoadingState.disconnect) {
-      _alert = Positioned(
-          bottom: 48.d,
-          child: Column(
-            children: [
-              Text(
-                "Connection Lost!\nPlease try again.",
-                textAlign: TextAlign.center,
-                style: TStyles.medium,
-              ),
-              SizedBox(height: 16.d),
-              Widgets.button(
-                  buttonId: -1,
-                  child: Text('Retry', style: TStyles.large),
-                  width: 180.d,
-                  color: TColors.blue,
-                  onPressed: () {
-                    _reload();
-                  })
-            ],
-          ));
-      setState(() {});
-      } else if (_services.connection.response.state == LoadingState.complete) {
-        _closeInput?.value = true;
-    }
-  } */
 
   void _reload() {
     close();
