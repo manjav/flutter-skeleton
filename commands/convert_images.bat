37c8adc159cc2eb3293f7deba7670b8c55fe79de
Mansour <mansurjavadi@gmail.com>
2023-06-26 19:33:57 +0300

    Add webp convertor prebuild command

---
+++ commands/convert_images.bat
@@ -0,0 +1,10 @@
+cd assets\\images
+for file in *.png;
+ do (
+    [ -e "%file%" ] || continue
+    echo \'%file%\' converted to \'%file%.*%.webp\'
+    cwebp %file% -lossless -m 6 -o %file%.*%.webp
+    rm %file%
+    );
+ done
+exit
