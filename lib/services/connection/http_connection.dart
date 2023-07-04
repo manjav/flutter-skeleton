d630fde913c1fa645f3a71e724c0ea1d62831aef
Mansour <mansurjavadi@gmail.com>
2023-07-04 11:23:48 +0300

    Add more RpcId

--- lib/services/connection/http_connection.dart
+++ lib/services/connection/http_connection.dart
@@ -185,13 +185,57 @@ enum ParamName {
   name
 }
 
-enum RpcId { battle, playerLoad }
+enum RpcId {
+  scout,
+  playerLoad,
+// Files
+  tutorialExport,
+  tutorialLangEnExport,
+  tutorialLangFaExport,
+  comboExport,
+  comboLangEnExport,
+  comboLangFaExport,
+// Hero APIs
+  heroItemsExport,
+  heroItemsLangEnExport,
+  heroItemsLangFaExport,
+  buyHeroItem,
+  setHeroItems,
+  fruitLangFaExport,
+  fruitLangEnExport,
+  imageStorageAPI_ImageStorage,
+  cardsExport,
+  captcha,
+  forgotPassword,
+  getVCBalance,
+}
 
 extension RpcIdEx on RpcId {
   String get value {
     return switch (this) {
-      RpcId.battle => "battle",
+      RpcId.scout => "battle/scout",
       RpcId.playerLoad => "player/load",
+      RpcId.tutorialExport => "metadata/TutorialData.json",
+      RpcId.tutorialLangEnExport => "i18n/en-US/Tutorial.json",
+      RpcId.tutorialLangFaExport => "i18n/fa-IR/Tutorial.json",
+      RpcId.comboExport => "metadata/CardComboData.json",
+      RpcId.comboLangEnExport => "i18n/en-US/CardComboLanguage.json",
+      RpcId.comboLangFaExport => "i18n/fa-IR/CardComboLanguage.json",
+      RpcId.heroItemsExport => "cards/heroitemsjsonexport",
+      RpcId.heroItemsLangEnExport => "i18n/en-US/BaseHeroItemsLanguage.json",
+      RpcId.heroItemsLangFaExport => "i18n/fa-IR/BaseHeroItemsLanguage.json",
+      RpcId.buyHeroItem => "store/buyheroitem",
+      RpcId.setHeroItems => "cards/equipheroitems",
+      RpcId.fruitLangFaExport => "i18n/fa-IR/BaseFruitLanguage.json",
+      RpcId.fruitLangEnExport => "i18n/en-US/BaseFruitLanguage.json",
+      RpcId.imageStorageAPI_ImageStorage => "cardpool/",
+      RpcId.cardsExport => "cards/cardsjsonexport",
+      RpcId.captcha => "bot/getcaptcha",
+      RpcId.forgotPassword => "user/iforgot",
+      RpcId.getVCBalance => "user/getvcbalance/client/iOS/"
+    };
+  }
+
     };
   }
 }
