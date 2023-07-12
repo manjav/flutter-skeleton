6dd18110da9a5922a6afd177e120c147f7f72eee
Mansour <mansurjavadi@gmail.com>
2023-07-12 11:08:33 +0300

    Add Building class

--- lib/data/core/building.dart
+++ lib/data/core/building.dart
@@ -10,4 +10,21 @@ enum Buildings {
   treasury,
   tribe,
   quest,
+  auction,
+}
+
+enum BuildingField { type, cards, level }
+
+class Building extends StringMap<dynamic> {
+  static const Building_GoldMinePowerModifier1 = 0.7; //per hour
+  static const Building_GoldMinePowerModifier2 = 3.0; //per hour
+  static const Building_OffensePowerModifier = 0.1;
+  static const Building_DefensePowerModifier = 0.1;
+  static const maxLevels = 10;
+
+  int get level => map['level'];
+  Buildings get type => map['type'];
+  List<int> get cards => map['cards'];
+
+}
 }
