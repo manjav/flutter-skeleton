a142b936f7f53cf3aa68905b33f3f70890db8146
Mansour <mansurjavadi@gmail.com>
2023-07-18 22:34:10 +0300

    Add flow for skinned text

--- lib/view/widgets/skinnedtext.dart
+++ lib/view/widgets/skinnedtext.dart
@@ -9,7 +9,7 @@ class SkinnedText extends StatelessWidget {
   final Color strokeColor;
   final double? strokeWidth;
   final Alignment alignment;
-
+  final TextOverflow? overflow;
   const SkinnedText(
     this.text, {
     Key? key,
@@ -17,6 +17,7 @@ class SkinnedText extends StatelessWidget {
     this.strokeColor = TColors.primary10,
     this.strokeWidth,
     this.alignment = Alignment.center,
+    this.overflow,
   }) : super(key: key);
 
   @override
@@ -24,6 +25,7 @@ class SkinnedText extends StatelessWidget {
     var style = this.style ?? TStyles.medium;
     return Stack(alignment: alignment, children: [
       Text(text,
+          overflow: overflow,
           style: style.copyWith(
               // height: 0.82,
               foreground: Paint()
@@ -32,9 +34,12 @@ class SkinnedText extends StatelessWidget {
                 ..style = PaintingStyle.stroke,
               shadows: [_shadow(0, style.fontSize! * 0.18)])),
       Text(text,
+          overflow: overflow,
           style: style.copyWith(
             // height: 0.9,
-            color: TColors.primary,
+            color: style.color == TColors.primary10
+                ? TColors.primary
+                : style.color,
           )),
       // SizedBox(height: style.fontSize! * 0.32)
     ]);
