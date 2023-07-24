451003f7cd8ba08cb9151515d6e48494a636dbc2
Mansour <mansurjavadi@gmail.com>
2023-07-24 20:17:33 +0300

    RpcId cleanup

--- lib/data/core/rpc.dart
+++ lib/data/core/rpc.dart
@@ -11,25 +11,9 @@ enum RpcId {
   scout,
   quest,
   battle,
-// Files
-  tutorialExport,
-  tutorialLangEnExport,
-  tutorialLangFaExport,
-  comboExport,
-  comboLangEnExport,
-  comboLangFaExport,
-// Hero APIs
-  heroItemsExport,
-  heroItemsLangEnExport,
-  heroItemsLangFaExport,
-  buyHeroItem,
-  setHeroItems,
-  fruitLangFaExport,
-  fruitLangEnExport,
-  imageStorageAPI_ImageStorage,
-  captcha,
-  forgotPassword,
-  getVCBalance,
+
+  // Building
+  upgrade,
 }
 
 extension RpcIdEx on RpcId {
@@ -43,23 +27,7 @@ extension RpcIdEx on RpcId {
       RpcId.battle => "battle/battle",
       RpcId.getOpponents => "battle/getopponents",
       RpcId.playerLoad => "player/load",
-      RpcId.tutorialExport => "metadata/TutorialData.json",
-      RpcId.tutorialLangEnExport => "i18n/en-US/Tutorial.json",
-      RpcId.tutorialLangFaExport => "i18n/fa-IR/Tutorial.json",
-      RpcId.comboExport => "metadata/CardComboData.json",
-      RpcId.comboLangEnExport => "i18n/en-US/CardComboLanguage.json",
-      RpcId.comboLangFaExport => "i18n/fa-IR/CardComboLanguage.json",
-      RpcId.heroItemsExport => "cards/heroitemsjsonexport",
-      RpcId.heroItemsLangEnExport => "i18n/en-US/BaseHeroItemsLanguage.json",
-      RpcId.heroItemsLangFaExport => "i18n/fa-IR/BaseHeroItemsLanguage.json",
-      RpcId.buyHeroItem => "store/buyheroitem",
-      RpcId.setHeroItems => "cards/equipheroitems",
-      RpcId.fruitLangFaExport => "i18n/fa-IR/BaseFruitLanguage.json",
-      RpcId.fruitLangEnExport => "i18n/en-US/BaseFruitLanguage.json",
-      RpcId.imageStorageAPI_ImageStorage => "cardpool/",
-      RpcId.captcha => "bot/getcaptcha",
-      RpcId.forgotPassword => "user/iforgot",
-      RpcId.getVCBalance => "user/getvcbalance/client/iOS/"
+      RpcId.upgrade => "tribe/upgrade"
     };
   }
 
@@ -71,24 +39,6 @@ extension RpcIdEx on RpcId {
 
   HttpRequestType get requestType {
     return switch (this) {
-      RpcId.tutorialExport ||
-      RpcId.tutorialLangEnExport ||
-      RpcId.tutorialLangFaExport ||
-      RpcId.comboExport ||
-      RpcId.comboLangEnExport ||
-      RpcId.comboLangFaExport ||
-      RpcId.heroItemsExport ||
-      RpcId.heroItemsLangEnExport ||
-      RpcId.heroItemsLangFaExport ||
-      RpcId.buyHeroItem ||
-      RpcId.setHeroItems ||
-      RpcId.fruitLangFaExport ||
-      RpcId.fruitLangEnExport ||
-      RpcId.imageStorageAPI_ImageStorage ||
-      RpcId.captcha ||
-      RpcId.forgotPassword ||
-      RpcId.getVCBalance =>
-        HttpRequestType.get,
       _ => HttpRequestType.post,
     };
   }
@@ -108,7 +58,6 @@ enum RpcParams {
   store_type,
   udid,
   // Quest - Battle
-  type,
   cards,
   hero_id,
   check,
@@ -116,5 +65,8 @@ enum RpcParams {
   attacks_in_today,
   // Cards
   card_id,
-  sacrifices
+  sacrifices,
+  // Buildings
+  type,
+  tribe_id
 }
