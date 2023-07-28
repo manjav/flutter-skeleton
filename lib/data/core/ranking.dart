22426642cfc3e2a910cfb1fce634517d54775100
Mansour <mansurjavadi@gmail.com>
2023-07-28 19:20:14 +0300

    Add rank data

--- lib/data/core/ranking.dart
+++ lib/data/core/ranking.dart
@@ -3,6 +3,89 @@ import 'dart:convert';
 import '../../services/prefs.dart';
 import '../../utils/utils.dart';
 import 'rpc.dart';
+
+class Ranks {
+  static List<T> createList<T extends Rank>(
+      Map<String, dynamic> map, int ownerId) {
+    var group = T.toString() == "Player" ? "players" : "tribes";
+    var result = _add<T>(map["top_$group"], ownerId);
+    var list = result.$1;
+    if (!result.$2) {
+      list.add(make<T>(null, 0));
+      list.addAll(_add<T>(map["near_$group"], ownerId).$1);
+    }
+    return list;
+  }
+
+  static (List<T>, bool) _add<T extends Rank>(List list, int ownerId) {
+    var hasOwner = false;
+    var result = <T>[];
+    var index = 0;
+    for (var item in list) {
+      var rank = make<T>(item as Map<String, dynamic>?, ownerId);
+      if (rank.itsMe) hasOwner = true;
+      rank.index = index++;
+      result.add(rank);
+    }
+    return (result, hasOwner);
+  }
+
+  /// Add factory functions for every Type and every constructor you want to make available to `make`
+  static final _factories = <Type, Function>{
+    Player: (Map<String, dynamic>? map, int ownerId) =>
+        Player.init(map, ownerId),
+    TribeRank: (Map<String, dynamic>? map, int ownerId) =>
+        TribeRank.init(map, ownerId),
+  };
+
+  static T make<T extends Rank>(Map<String, dynamic>? map, int ownerId) {
+    return _factories[T]!(map, ownerId);
+  }
+
+  static final Map<RpcId, List<Rank>?> lists = {};
+}
+
+abstract class Rank {
+  int index = 0, id = 0, rank = 0, score = 0;
+  String name = "";
+  late final bool itsMe;
+  Rank.init(Map<String, dynamic>? map, int ownerId) {
+    if (map == null) return;
+    id = map["id"] ?? 0;
+    name = map["name"] ?? "";
+    rank = map["rank"] ?? 0;
+    itsMe = ownerId == id;
+  }
+}
+
+class TribeRank extends Rank {
+  int memberCount = 0, weeklyScore = 0, weeklyRank = 0;
+  String description = "";
+  TribeRank.init(Map<String, dynamic>? map, int ownerId)
+      : super.init(map, ownerId) {
+    if (map == null) return;
+    score = map["score"] ?? 0;
+    description = map["description"] ?? "";
+    memberCount = map["member_count"] ?? 0;
+    weeklyScore = map["weekly_score"] ?? 0;
+    weeklyRank = map["weekly_rank"] ?? 0;
+  }
+}
+
+class Player extends Rank {
+  String tribeName = "";
+  int level = 0, tribeId = 0, avatarId = 0;
+  Player.init(Map<String, dynamic>? map, int ownerId)
+      : super.init(map, ownerId) {
+    if (map == null) return;
+    score = map["xp"] ?? 0;
+    level = map["level"] ?? 0;
+    tribeId = map["tribe_id"] ?? 0;
+    tribeName = map["tribe_name"] ?? "";
+    avatarId = map["avatar_id"] ?? 0;
+  }
+}
+
 class Opponent extends Player {
   static int scoutCost = 0;
   static Map<String, dynamic> _attackLogs = {};
