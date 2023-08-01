04328ec0662c59b4253ddd17a6f15f2763cbbe38
Mansour <mansurjavadi@gmail.com>
2023-08-01 17:09:42 +0300

    Add hero items

--- lib/view/popups/hero_popup.dart
+++ lib/view/popups/hero_popup.dart
@@ -25,21 +25,38 @@ class _HeroPopupState extends AbstractPopupState<HeroPopup> {
   int _selectedIndex = 0;
 
   late Account _account;
-  late List<AccountCard> _cards;
+  late List<HeroCard> _cards;
   late List<GlobalKey> _keys;
+  List<BaseHeroItem> _minions = [];
+  List<BaseHeroItem> _weapons = [];
+  late Map<int, BaseHeroItem> _baseHeroItems;
+  Map<String, String> benefits = {
+    "blessing": "benefit_gold",
+    "power": "benefit_power",
+    "wisdom": "benefit_cooldown"
+  };
 
   @override
   void initState() {
     super.initState();
     _account = BlocProvider.of<AccountBloc>(context).account!;
-    _cards = _account.getCards().values.where((card) => card.isHero).toList();
+    _cards =
+        _account.get<Map<int, HeroCard>>(AccountField.heroes).values.toList();
     _keys = List.generate(_cards.length, (index) => GlobalKey());
+    _baseHeroItems = _account.get(AccountField.base_heroitems);
+    for (var item in _baseHeroItems.values) {
+      if (item.category == 1) {
+        _minions.add(item);
+      } else {
+        _weapons.add(item);
+      }
+    }
   }
 
   @override
   contentFactory() {
-    var card = _cards[_selectedIndex];
-    var name = card.base
+    var hero = _cards[_selectedIndex];
+    var name = hero.card.base
         .get<FruitData>(CardFields.fruit)
         .get<String>(FriutFields.name);
     return SizedBox(
@@ -69,22 +86,61 @@ class _HeroPopupState extends AbstractPopupState<HeroPopup> {
                 child: Asset.load<Image>('arrow_right')),
           ],
         ),
-        SizedBox(height: 32.d),
-        Widgets.rect(
-            radius: 32.d,
-            color: TColors.primary90,
-            padding: EdgeInsets.all(16.d),
-            child: Row(mainAxisSize: MainAxisSize.min, children: [
-              // Asset.load<Image>("icon_gold", wi5dth: 72.d),
-              // SizedBox(width: 12.d),
-              // SkinnedText("combo_$index".l()),
-            ])),
+        SizedBox(height: 24.d),
+        _attributesBuilder(hero),
+        Text("minions_l".l()),
+        _itemsListBuilder(_minions),
+        Text("weapons_l".l()),
+        _itemsListBuilder(_weapons),
       ]),
     );
   }
 
-  _setIndex(int offset) {
+  void _setIndex(int offset) {
     _selectedIndex = (_selectedIndex + offset) % _cards.length;
     setState(() {});
+  }
+
+  Widget _attributesBuilder(HeroCard hero) {
+    var attributes = hero.getGainedAttributesByItems();
+    return Widgets.rect(
+        radius: 32.d,
+        width: 640.d,
+        color: TColors.primary90,
+        padding: EdgeInsets.all(16.d),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              for (var attribute in attributes.entries)
+                _attributeBuilder(hero, attribute)
+            ]));
+  }
+
+  Widget _attributeBuilder(HeroCard hero, MapEntry<String, int> attribute) {
+    return Row(children: [
+      Asset.load<Image>(benefits[attribute.key]!, width: 56.d),
+      SkinnedText(" ${hero.card.base.map['${attribute.key}Attribute']}"),
+      SkinnedText(" + ${attribute.value}",
+          style: TStyles.medium.copyWith(color: TColors.green)),
+    ]);
+  }
+
+  _itemsListBuilder(List<BaseHeroItem> items) {
+    return Widgets.rect(
+        height: 200.d,
+        width: 900.d,
+        child: ListView.builder(
+          itemExtent: 200.d,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (c, i) => _itemBuilder(i, items[i]),
+          itemCount: items.length,
+        ));
+  }
+
+  Widget? _itemBuilder(int index, BaseHeroItem item) {
+    return Widgets.button(
+        color: TColors.accent,
+        margin: EdgeInsets.all(12.d),
+        child: Asset.load<Image>("heroitem_${item.image}"));
   }
 }
