ffacc5bc25c4335b4c1de38c2618807259a067f2
Mansour <mansurjavadi@gmail.com>
2023-08-02 18:17:36 +0300

    Add `ValueNotifier`

--- lib/view/popups/combo_popup.dart
+++ lib/view/popups/combo_popup.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_skeleton/services/deviceinfo.dart';
-import 'package:flutter_skeleton/services/localization.dart';
-import 'package:flutter_skeleton/services/theme.dart';
-import 'package:flutter_skeleton/view/widgets/skinnedtext.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../blocs/account_bloc.dart';
+import '../../data/core/account.dart';
+import '../../services/deviceinfo.dart';
+import '../../services/localization.dart';
+import '../../services/theme.dart';
 import '../../utils/assets.dart';
 import '../../view/popups/ipopup.dart';
+import '../../view/widgets/skinnedtext.dart';
 import '../route_provider.dart';
 import '../widgets.dart';
+import '../widgets/indicator.dart';
 
 class ComboPopup extends AbstractPopup {
   const ComboPopup({super.key, required super.args}) : super(Routes.popupCombo);
@@ -17,11 +21,14 @@ class ComboPopup extends AbstractPopup {
 }
 
 class _ComboPopupState extends AbstractPopupState<ComboPopup> {
-  int _selectedCombo = 0;
+  late Account _account;
+  final ValueNotifier<int> _selectedIndex = ValueNotifier(1);
 
   @override
   void initState() {
+    contentPadding = EdgeInsets.fromLTRB(48.d, 176.d, 48.d, 72.d);
     super.initState();
+    _account = BlocProvider.of<AccountBloc>(context).account!;
   }
 
   @override
@@ -35,26 +42,48 @@ class _ComboPopupState extends AbstractPopupState<ComboPopup> {
 
   @override
   contentFactory() {
+    var style = TStyles.medium.copyWith(fontSize: 36.d);
+    var style2 = TStyles.medium.copyWith(fontSize: 36.d, color: TColors.orange);
     return SizedBox(
       width: 920.d,
-      height: DeviceInfo.size.height * 0.7,
-      child: Column(children: [
+      height: 1300.d,
+      child: ValueListenableBuilder(
+          valueListenable: _selectedIndex,
+          builder: (context, value, child) {
+            var combo = _account.loadingData.comboHints[value];
+            return Column(children: [
+              SizedBox(height: 444.d),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Asset.load<Image>("icon_potion_number", height: 110.d),
+                SkinnedText(" x${combo.cost}")
+              ]),
+              SizedBox(height: 24.d),
+              Text("combo_${value}_description".l(),
+                  style: TStyles.medium.copyWith(height: 1)),
+              const Expanded(child: SizedBox()),
         Wrap(
           alignment: WrapAlignment.center,
           runSpacing: 16.d,
           spacing: 16.d,
-          children: [for (var i = 0; i < 8; i++) _comboItem(i)],
-        )
-      ]),
+                  children: [
+                    for (var i = 1;
+                        i < _account.loadingData.comboHints.length;
+                        i++)
+                      _comboItem(i)
+                  ])
+            ]);
+          }),
     );
   }
 
   _comboItem(int index) {
+    var selected = _selectedIndex.value == index;
     return Widgets.button(
+        height: 110.d,
         radius: 32.d,
-        color: TColors.primary90,
+        color: selected ? TColors.primary80 : TColors.primary90,
         padding: EdgeInsets.all(16.d),
-        foregroundDecoration: _selectedCombo == index
+        foregroundDecoration: selected
             ? BoxDecoration(
                 borderRadius: BorderRadius.all(Radius.circular(32.d)),
                 border: Border.all(color: TColors.primary10, width: 8.d))
@@ -64,6 +93,6 @@ class _ComboPopupState extends AbstractPopupState<ComboPopup> {
           SizedBox(width: 12.d),
           SkinnedText("combo_$index".l()),
         ]),
-        onPressed: () => setState(() => _selectedCombo = index));
+        onPressed: () => setState(() => _selectedIndex.value = index));
   }
 }
