f63d8935c7f6f7303f14f8317fce2ef0845ec656
Mansour <mansurjavadi@gmail.com>
2023-08-02 18:38:18 +0300

    Show unavailable state of combo

--- lib/view/popups/combo_popup.dart
+++ lib/view/popups/combo_popup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_skeleton/data/core/card.dart';
 
 import '../../blocs/account_bloc.dart';
 import '../../data/core/account.dart';
@@ -44,6 +45,7 @@ class _ComboPopupState extends AbstractPopupState<ComboPopup> {
   contentFactory() {
     var style = TStyles.medium.copyWith(fontSize: 36.d);
     var style2 = TStyles.medium.copyWith(fontSize: 36.d, color: TColors.orange);
+    var comboCount = _account.loadingData.comboHints.length;
     return SizedBox(
       width: 920.d,
       height: 1300.d,
@@ -51,44 +53,54 @@ class _ComboPopupState extends AbstractPopupState<ComboPopup> {
           valueListenable: _selectedIndex,
           builder: (context, value, child) {
             var combo = _account.loadingData.comboHints[value];
-            return Column(children: [
-              SizedBox(height: 444.d),
-              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                Asset.load<Image>("icon_potion_number", height: 110.d),
-                SkinnedText(" x${combo.cost}")
-              ]),
-              SizedBox(height: 24.d),
-              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                SkinnedText("combo_suit_level".l(), style: style),
-                SkinnedText("combo_suit_${combo.level}".l(), style: style2),
-                SizedBox(width: 32.d),
-                SkinnedText("combo_suit_power".l(), style: style),
-                SkinnedText("combo_suit_${combo.power}".l(), style: style2),
-                SizedBox(width: 32.d),
-                SkinnedText("combo_suit_fruit".l(), style: style),
-                SkinnedText("combo_suit_${combo.fruit}".l(), style: style2),
-              ]),
-              SizedBox(height: 24.d),
-              Text("combo_${value}_description".l(),
-                  style: TStyles.medium.copyWith(height: 1)),
-              const Expanded(child: SizedBox()),
-              Wrap(
-                  alignment: WrapAlignment.center,
-                  runSpacing: 16.d,
-                  spacing: 16.d,
-                  children: [
-                    for (var i = 1;
-                        i < _account.loadingData.comboHints.length;
-                        i++)
-                      _comboItem(i)
-                  ])
-            ]);
+            var items = <Widget>[SizedBox(height: 444.d)];
+            items.addAll(_revealedItemsBuilder(combo, style, style2));
+            items.add(const Expanded(child: SizedBox()));
+            items.add(Wrap(
+                alignment: WrapAlignment.center,
+                runSpacing: 16.d,
+                spacing: 16.d,
+                children: [
+                  for (var i = 1; i < comboCount; i++) _comboItem(i)
+                ]));
+            return Column(children: items);
           }),
     );
   }
 
+  List<Widget> _revealedItemsBuilder(
+      ComboHint combo, TextStyle style, TextStyle style2) {
+    if (!combo.isAvailable) {
+      return <Widget>[
+        SizedBox(height: 124.d),
+        Text("combo_unavailable_description".l())
+      ];
+    }
+    return [
+      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Asset.load<Image>("icon_potion_number", height: 110.d),
+        SkinnedText(" x${combo.cost}")
+      ]),
+      SizedBox(height: 24.d),
+      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        SkinnedText("combo_suit_level".l(), style: style),
+        SkinnedText("combo_suit_${combo.level}".l(), style: style2),
+        SizedBox(width: 32.d),
+        SkinnedText("combo_suit_power".l(), style: style),
+        SkinnedText("combo_suit_${combo.power}".l(), style: style2),
+        SizedBox(width: 32.d),
+        SkinnedText("combo_suit_fruit".l(), style: style),
+        SkinnedText("combo_suit_${combo.fruit}".l(), style: style2),
+      ]),
+      SizedBox(height: 24.d),
+      Text("combo_${combo.id}_description".l(),
+          style: TStyles.medium.copyWith(height: 1)),
+    ];
+  }
+
   _comboItem(int index) {
     var selected = _selectedIndex.value == index;
+    var combo = _account.loadingData.comboHints[index];
     return Widgets.button(
         height: 110.d,
         radius: 32.d,
@@ -100,7 +112,9 @@ class _ComboPopupState extends AbstractPopupState<ComboPopup> {
                 border: Border.all(color: TColors.primary10, width: 8.d))
             : null,
         child: Row(mainAxisSize: MainAxisSize.min, children: [
-          Asset.load<Image>("icon_gold", width: 72.d),
+          Opacity(
+              opacity: combo.isAvailable ? 1 : 0.4,
+              child: Asset.load<Image>("icon_gold", width: 72.d)),
           SizedBox(width: 12.d),
           SkinnedText("combo_$index".l()),
         ]),
