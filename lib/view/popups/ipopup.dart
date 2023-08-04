26df8f1b7b611ede087b47dec942c70e4713acf0
Mansour <mansurjavadi@gmail.com>
2023-08-04 16:59:50 +0300

    Customizable alignment of popups

--- lib/view/popups/ipopup.dart
+++ lib/view/popups/ipopup.dart
@@ -27,6 +27,7 @@ class AbstractPopup extends StatefulWidget {
 
 class AbstractPopupState<T extends AbstractPopup> extends State<T>
     with ILogger {
+  Alignment alignment = Alignment.center;
   EdgeInsets contentPadding = EdgeInsets.fromLTRB(48.d, 176.d, 48.d, 92.d);
 
   @override
@@ -38,28 +39,29 @@ class AbstractPopupState<T extends AbstractPopup> extends State<T>
       body: Stack(children: [
         Widgets.touchable(onTap: () => Navigator.pop(context)),
         Align(
+            alignment: alignment,
             child: Widgets.rect(
-          margin: EdgeInsets.fromLTRB(24.d, 100.d, 24.d, 0),
-          padding: EdgeInsets.symmetric(horizontal: 24.d),
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  fit: BoxFit.fill,
-                  centerSlice: chromeCenterSlice.centerSlice,
-                  image: Asset.load<Image>(
-                    'popup_chrome',
-                    centerSlice: chromeCenterSlice,
-                  ).image)),
-          child: Stack(
-              alignment: Alignment.topCenter,
-              fit: StackFit.loose,
-              children: [
-                innerChromeFactory(),
-                titleTextFactory(),
-                Padding(padding: contentPadding, child: contentFactory()),
-                Positioned(
-                    top: 60.d, right: -12.d, child: closeButtonFactory()),
-              ]),
-        )),
+              margin: EdgeInsets.fromLTRB(24.d, 100.d, 24.d, 0),
+              padding: EdgeInsets.symmetric(horizontal: 24.d),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      centerSlice: chromeCenterSlice.centerSlice,
+                      image: Asset.load<Image>(
+                        'popup_chrome',
+                        centerSlice: chromeCenterSlice,
+                      ).image)),
+              child: Stack(
+                  alignment: Alignment.topCenter,
+                  fit: StackFit.loose,
+                  children: [
+                    innerChromeFactory(),
+                    titleTextFactory(),
+                    Padding(padding: contentPadding, child: contentFactory()),
+                    Positioned(
+                        top: 60.d, right: -12.d, child: closeButtonFactory()),
+                  ]),
+            )),
         Positioned(
             top: 16.d,
             left: 32.d,
