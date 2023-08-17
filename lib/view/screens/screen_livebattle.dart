2921a373dee6511635dd9b75ab705d32972d2758
Mansour <mansurjavadi@gmail.com>
2023-08-17 22:26:57 +0300

    Add deploy  holders

--- lib/view/screens/screen_livebattle.dart
+++ lib/view/screens/screen_livebattle.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../blocs/account_bloc.dart';
+import '../../data/core/account.dart';
+import '../../data/core/card.dart';
+import '../../services/deviceinfo.dart';
+import '../../view/widgets/card_holder.dart';
 import '../route_provider.dart';
 import '../widgets.dart';
 import '../widgets/power_balance.dart';
+import '../widgets/live_battle/livebattle_deploy_holder.dart';
+import '../widgets/live_battle/power_balance.dart';
 import 'iscreen.dart';
 
 class LiveBattleScreen extends AbstractScreen {
@@ -45,6 +53,14 @@ class _LiveBattleScreenState extends AbstractScreenState<AbstractScreen> {
                     valueListenable: _powerBalance,
                     builder: (context, value, child) =>
                         Powerbalance(value, _maxPower))),
+            DeployHolder(0, -0.85, -0.25, 0.2, _enemyDeployedCards),
+            DeployHolder(1, -0.3, -0.21, 0.1, _enemyDeployedCards),
+            DeployHolder(2, 0.3, -0.21, -0.1, _enemyDeployedCards),
+            DeployHolder(3, 0.85, -0.25, -0.2, _enemyDeployedCards),
+            DeployHolder(0, -0.85, 0.25, -0.2, _myDeloyedCards),
+            DeployHolder(1, -0.3, 0.21, -0.1, _myDeloyedCards),
+            DeployHolder(2, 0.3, 0.21, 0.1, _myDeloyedCards),
+            DeployHolder(3, 0.85, 0.25, 0.2, _myDeloyedCards),
         ));
   }
 
---
+++ lib/view/widgets/live_battle/livebattle_deploy_holder.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import '../../../services/deviceinfo.dart';
+
+import '../../../data/core/card.dart';
+import '../../../utils/assets.dart';
+import '../../items/card_item.dart';
+import '../../key_provider.dart';
+import '../card_holder.dart';
+
+class DeployHolder extends StatelessWidget with KeyProvider {
+  final int index;
+  final double alignX, alignY, rotation;
+  final SelectedCards deployedCards;
+
+  DeployHolder(
+      this.index, this.alignX, this.alignY, this.rotation, this.deployedCards,
+      {super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<List<AccountCard?>>(
+        valueListenable: deployedCards,
+        builder: (context, value, child) {
+          var size = 180.d;
+          Widget? card;
+          if (value[index] != null) {
+            if (value[index]!.id == -1) {
+              card = Asset.load<Image>("deck_live_missed");
+            } else {
+              card = CardItem(value[index]!,
+                  size: size,
+                  showCooldown: false,
+                  key: getGlobalKey(value[index]!.id));
+            }
+          } else {
+            card = Asset.load<Image>("deck_live_empty");
+          }
+          return Align(
+              alignment: Alignment(alignX, alignY),
+              child: Transform.rotate(
+                  angle: rotation,
+                  child: SizedBox(
+                      width: size,
+                      height: size / CardItem.aspectRatio,
+                      child: card)));
+        });
+  }
+}
