ec453fd083a0d1d9c1844ac4789360b67c44983e
Mansour <mansurjavadi@gmail.com>
2023-08-17 22:28:08 +0300

    Add live-battle deck

--- lib/view/screens/screen_livebattle.dart
+++ lib/view/screens/screen_livebattle.dart
@@ -8,7 +8,7 @@ import '../../services/deviceinfo.dart';
 import '../../view/widgets/card_holder.dart';
 import '../route_provider.dart';
 import '../widgets.dart';
-import '../widgets/power_balance.dart';
+import '../widgets/live_battle/live_deck.dart';
 import '../widgets/live_battle/livebattle_deploy_holder.dart';
 import '../widgets/live_battle/power_balance.dart';
 import 'iscreen.dart';
@@ -22,11 +22,16 @@ class LiveBattleScreen extends AbstractScreen {
 
 class _LiveBattleScreenState extends AbstractScreenState<AbstractScreen> {
   late Account _account;
+  late PageController _pageController;
   final SelectedCards _myDeloyedCards = SelectedCards([null, null, null, null]);
   final SelectedCards _enemyDeployedCards =
       SelectedCards([null, null, null, null]);
   final ValueNotifier<int> _powerBalance = ValueNotifier(0);
   int _maxPower = 0;
+  List<AccountCard> _cards = [];
+
+  int get _readySlotIndex => 0;
+
   @override
   List<Widget> appBarElementsLeft() => [];
   @override
@@ -37,7 +42,16 @@ class _LiveBattleScreenState extends AbstractScreenState<AbstractScreen> {
     _account = BlocProvider.of<AccountBloc>(context).account!;
     _cards = _account.getReadyCards();
     _maxPower = _account.get<int>(AccountField.def_power);
+    _pageController = PageController(viewportFraction: 0.3, keepPage: true);
     super.initState();
+    WidgetsBinding.instance
+        .addPostFrameCallback((d) => _pageController.jumpToPage(4));
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
@@ -61,20 +75,20 @@ class _LiveBattleScreenState extends AbstractScreenState<AbstractScreen> {
             DeployHolder(1, -0.3, 0.21, -0.1, _myDeloyedCards),
             DeployHolder(2, 0.3, 0.21, 0.1, _myDeloyedCards),
             DeployHolder(3, 0.85, 0.25, 0.2, _myDeloyedCards),
+            LiveDeck(_pageController, _cards, _onCardFocusChanged),
         ));
-  }
-
-  _deckItemBuilder(int index, double alignX, double alignY, double rotation) {
-    return Align(
-        alignment: Alignment(alignX + 0.05, alignY),
-        child: Transform.rotate(
-            angle: rotation + Random().nextDouble() * 0.16 - 0.08,
-            child: Asset.load<Image>(
-                "deck_live_${index > 1 ? "missed" : "empty"}",
-                width: 200.d)));
   }
   
   void _onCardFocusChanged(int index, AccountCard focusedCard) {
-    print(index);
+    if (focusedCard.base.isHero) {
+      _myDeloyedCards.setAtCard(_readySlotIndex, null);
+    } else {
+      _myDeloyedCards.setAtCard(_readySlotIndex, focusedCard);
+    }
+    var powerBalance = 0;
+    for (var card in _myDeloyedCards.value) {
+      powerBalance += card != null ? card.power : 0;
+    }
+    _powerBalance.value = powerBalance;
   }
 }
--- lib/view/widgets/live_battle/live_deck.dart
+++ lib/view/widgets/live_battle/live_deck.dart
@@ -2,10 +2,10 @@ import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 
-import '../../data/core/card.dart';
-import '../../services/deviceinfo.dart';
-import '../../view/items/card_item.dart';
-import '../widgets.dart';
+import '../../../data/core/card.dart';
+import '../../../services/deviceinfo.dart';
+import '../../items/card_item.dart';
+import '../../widgets.dart';
 
 class LiveDeck extends StatelessWidget {
   final List<AccountCard> items;
@@ -35,34 +35,33 @@ class LiveDeck extends StatelessWidget {
   _cardBuilder(BuildContext context, int index, double size) {
     var delta = 1.0, angle = 0.0, scale = 1.0, deltaX = 0.0, deltaY = 0.0;
     return AnimatedBuilder(
-      animation: pageController,
-      builder: (context, child) {
-        if (pageController.position.haveDimensions) {
-          delta = (pageController.page! - index);
-          angle = delta / -10;
-          scale = 0.2 + (0.6 / delta).abs().clamp(0, 0.8);
-          deltaX = math.pow(delta, 3) * 8;
-          deltaY = delta.abs() * 32;
-        }
-        return Transform.translate(
-          offset: Offset(deltaX, deltaY),
-          child: Transform.rotate(
-            angle: angle,
-            child: Transform.scale(
-              scale: scale,
-              child: Opacity(
-                opacity: scale,
-                child: Widgets.button(
-                    padding: EdgeInsets.zero,
-                    onPressed: () => _onCardTap(context, index),
-                    child: CardItem(items[index],
-                        size: size, showCooldown: false, showCooloff: true)),
+        animation: pageController,
+        builder: (context, child) {
+          if (pageController.position.haveDimensions) {
+            delta = (pageController.page! - index);
+            angle = delta / -10;
+            scale = 0.2 + (0.6 / delta).abs().clamp(0, 0.8);
+            deltaX = math.pow(delta, 3) * 8;
+            deltaY = delta.abs() * 32;
+          }
+          return Transform.translate(
+            offset: Offset(deltaX, deltaY),
+            child: Transform.rotate(
+              angle: angle,
+              child: Transform.scale(
+                scale: scale,
+                child: Opacity(
+                  opacity: scale,
+                  child: Widgets.button(
+                      padding: EdgeInsets.zero,
+                      onPressed: () => _onCardTap(context, index),
+                      child: CardItem(items[index],
+                          size: size, showCooldown: false, showCooloff: true)),
+                ),
               ),
             ),
-          ),
-        );
-      },
-    );
+          );
+        });
   }
 
   _onCardTap(BuildContext context, int index) {
