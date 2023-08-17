1e72a9ae00a7239437a17220ad7a88648e4bd854
Mansour <mansurjavadi@gmail.com>
2023-08-17 22:16:11 +0300

    Add power balance slider

--- lib/view/screens/screen_livebattle.dart
+++ lib/view/screens/screen_livebattle.dart
@@ -13,10 +13,24 @@ class LiveBattleScreen extends AbstractScreen {
 }
 
 class _LiveBattleScreenState extends AbstractScreenState<AbstractScreen> {
+  late Account _account;
+  final SelectedCards _myDeloyedCards = SelectedCards([null, null, null, null]);
+  final SelectedCards _enemyDeployedCards =
+      SelectedCards([null, null, null, null]);
+  final ValueNotifier<int> _powerBalance = ValueNotifier(0);
+  int _maxPower = 0;
   @override
   List<Widget> appBarElementsLeft() => [];
   @override
   List<Widget> appBarElementsRight() => [];
+
+  @override
+  void initState() {
+    _account = BlocProvider.of<AccountBloc>(context).account!;
+    _cards = _account.getReadyCards();
+    _maxPower = _account.get<int>(AccountField.def_power);
+    super.initState();
+  }
 
   @override
   Widget contentFactory() {
@@ -25,16 +39,12 @@ class _LiveBattleScreenState extends AbstractScreenState<AbstractScreen> {
         child: Stack(
           alignment: Alignment.center,
           children: [
-            const Positioned(left: 0, child: Powerbalance()),
-            _deckItemBuilder(0, -0.85, -0.25, 0.2),
-            _deckItemBuilder(1, -0.3, -0.21, 0.1),
-            _deckItemBuilder(2, 0.3, -0.21, -0.1),
-            _deckItemBuilder(3, 0.85, -0.25, -0.2),
-            _deckItemBuilder(0, -0.85, 0.25, -0.2),
-            _deckItemBuilder(1, -0.3, 0.21, -0.1),
-            _deckItemBuilder(2, 0.3, 0.21, 0.1),
-            _deckItemBuilder(3, 0.85, 0.25, 0.2),
-            LiveDeck(_pageController, _readyCards, _onCardFocusChanged),
+            Positioned(
+                left: -48.d,
+                child: ValueListenableBuilder<int>(
+                    valueListenable: _powerBalance,
+                    builder: (context, value, child) =>
+                        Powerbalance(value, _maxPower))),
         ));
   }
 
--- lib/view/widgets/power_balance.dart
+++ lib/view/widgets/power_balance.dart
@@ -1,34 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_skeleton/utils/utils.dart';
+import 'package:flutter_skeleton/view/widgets/skinnedtext.dart';
 import '../../services/deviceinfo.dart';
 import '../../services/theme.dart';
 
 import '../widgets.dart';
 
-class Powerbalance extends StatefulWidget {
-  const Powerbalance({super.key});
-
-  @override
-  State<Powerbalance> createState() => _PowerbalanceState();
-}
-
-class _PowerbalanceState extends State<Powerbalance>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+class Powerbalance extends StatelessWidget {
+  final int value, maxValue;
+  const Powerbalance(this.value, this.maxValue, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Widgets.rect(color: TColors.accent, width: 32.d, height: 800.d);
+    var p = 8.d;
+    var w = 36.d;
+    var r = 56.d;
+    var c = 100.d;
+    var h = DeviceInfo.size.height - 800.d;
+    var height = (1 - (value.abs() / maxValue)) * h * 0.5;
+    var color = value == 0
+        ? TColors.transparent
+        : value > 0
+            ? TColors.green
+            : TColors.accent;
+    var bgColor = TColors.primary10;
+    return Stack(
+        alignment: Alignment.center,
+        clipBehavior: Clip.none,
+        children: [
+          Widgets.rect(width: w, height: h, color: bgColor, radius: r),
+          Widgets.rect(width: c, height: c, radius: r, color: bgColor),
+          Widgets.rect(
+              width: c - p * 2, height: c - p * 2, radius: r, color: color),
+          AnimatedPositioned(
+              bottom: value > 0 ? p + height : h * 0.5,
+              top: value < 0 ? p + height : h * 0.5,
+              curve: Curves.easeOutCubic,
+              duration: const Duration(seconds: 1),
+              child:
+                  Widgets.rect(width: w - p * 2, color: color, radius: r - p)),
+          Positioned(left: 132.d, child: SkinnedText(value.compact()))
+        ]);
   }
 }
