074b22f2d3c429c29aab3232d4fe12241bd78741
Mansour <mansurjavadi@gmail.com>
2023-08-17 15:56:38 +0300

    Add deck placeholders

--- lib/view/screens/screen_livebattle.dart
+++ lib/view/screens/screen_livebattle.dart
@@ -26,8 +26,25 @@ class _LiveBattleScreenState extends AbstractScreenState<AbstractScreen> {
           alignment: Alignment.center,
           children: [
             const Positioned(left: 0, child: Powerbalance()),
+            _deckItemBuilder(0, -0.85, -0.25, 0.2),
+            _deckItemBuilder(1, -0.3, -0.21, 0.1),
+            _deckItemBuilder(2, 0.3, -0.21, -0.1),
+            _deckItemBuilder(3, 0.85, -0.25, -0.2),
+            _deckItemBuilder(0, -0.85, 0.25, -0.2),
+            _deckItemBuilder(1, -0.3, 0.21, -0.1),
+            _deckItemBuilder(2, 0.3, 0.21, 0.1),
+            _deckItemBuilder(3, 0.85, 0.25, 0.2),
         ));
   }
 
+  _deckItemBuilder(int index, double alignX, double alignY, double rotation) {
+    return Align(
+        alignment: Alignment(alignX, alignY),
+        child: Transform.rotate(
+            angle: rotation + Random().nextDouble() * 0.16 - 0.08,
+            child: Asset.load<Image>(
+                "deck_live_${index > 1 ? "missed" : "empty"}",
+                width: 200.d)));
+  }
   
 }
