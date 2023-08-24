4bce77667e89c0922cd530939ccde0391d0653c0
Mansour <mansurjavadi@gmail.com>
2023-08-24 20:49:42 +0300

    Cache account data

--- lib/services/connection/socket.dart
+++ lib/services/connection/socket.dart
@@ -1,10 +1,11 @@
 import 'dart:convert';
 
-import 'package:flutter_skeleton/data/core/account.dart';
-import 'package:flutter_skeleton/data/core/rpc_data.dart';
-import 'package:flutter_skeleton/services/iservices.dart';
 import 'package:tcp_socket_connection/tcp_socket_connection.dart';
 
+import '../../data/core/account.dart';
+import '../../data/core/card.dart';
+import '../../data/core/rpc_data.dart';
+import '../../services/iservices.dart';
 import '../../utils/utils.dart';
 
 enum NoobCommand { subscribe, unsubscribe }
@@ -13,21 +14,22 @@ extension NoobCommandExtension on NoobCommand {
   String get value => name.toUpperCase();
 }
 
-
 class NoobSocket extends IService {
   late TcpSocketConnection _socketConnection;
+
+  late Account _account;
   String get _secret => "floatint201412bool23string";
 
   @override
   initialize({List<Object>? args}) async {
     super.initialize(args: args);
-    var account = args![0] as Account;
+    _account = args![0] as Account;
 
     _socketConnection =
         TcpSocketConnection(LoadingData.chatIp, LoadingData.chatPort);
     // _socketConnection.enableConsolePrint(true);
     await _socketConnection.connect(500, _messageReceived, attempts: 3);
-    subscribe("user${account.get(AccountField.id)}");
+    subscribe("user${_account.get(AccountField.id)}");
   }
 
   void _messageReceived(String message) {
