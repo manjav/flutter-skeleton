cce1c2ef86be4b23b2f158f7d8b7b30641fb122a
Mansour <mansurjavadi@gmail.com>
2023-09-04 21:02:49 +0300

    Add team

--- lib/view/screens/screen_livebattle_outcome.dart
+++ lib/view/screens/screen_livebattle_outcome.dart
@@ -73,36 +73,62 @@ class _LiveOutScreenState extends AbstractScreenState<LiveOutScreen> {
         ]);
   }
 
-  Widget _fractionBuilder(
-      String fraction, OpponentResult opponent, List<OpponentResult> team) {
+  Widget _fractionBuilder(OpponentResult opponent, List<OpponentResult> team) {
     var sliceData = ImageCenterSliceDate(201, 158);
     return Widgets.rect(
         alignment: Alignment.center,
-        padding: EdgeInsets.fromLTRB(80.d, 50.d, 0, 10.d),
+        padding: EdgeInsets.fromLTRB(80.d, 90.d, 80.d, 60.d),
         decoration: BoxDecoration(
             image: DecorationImage(
-                image: Asset.load<Image>("liveout_bg_$fraction",
+                image: Asset.load<Image>("liveout_bg_${opponent.fraction.name}",
                         centerSlice: sliceData)
                     .image,
                 centerSlice: sliceData.centerSlice)),
-        height: 580.d,
-        child: SingleChildScrollView(
-            child: SizedBox(
-                width: 400.d,
-                height: 440.d,
-                child: Row(
+        height: 680.d,
+        child: team.isEmpty
+            ? _ownerBuilder(opponent, isExpanded: true)
+            : Row(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [_ownerBuilder(fraction, opponent)],
-                ))));
+                children: [
+                  _ownerBuilder(opponent, isExpanded: true),
+                  SizedBox(width: 20.d),
+                  _helpersBuilder(team),
+                ],
+              ));
   }
 
-  Widget _ownerBuilder(String fraction, OpponentResult opponent) {
+  Widget _helpersBuilder(List<OpponentResult> team) {
+    return Expanded(
+        child: GridView.builder(
+      scrollDirection: Axis.horizontal,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        childAspectRatio: 0.5,
+        crossAxisCount: 3,
+      ),
+      itemCount: team.length,
+      itemBuilder: (context, index) => _ownerBuilder(team[index]),
+    ));
+  }
+
+  Widget _ownerBuilder(OpponentResult opponent, {bool isExpanded = false}) {
     var sliceData =
         ImageCenterSliceDate(68, 92, const Rect.fromLTWH(32, 34, 4, 4));
-
+    var items = <Widget>[_titleBuilder(opponent)];
+    if (isExpanded) {
+      items.addAll([
+        SizedBox(height: 10.d),
+        _rewardItemBuilder("icon_seed", opponent.score, "power", opponent),
+        _rewardItemBuilder("icon_gold", opponent.gold, "gold", opponent),
+        _rewardItemBuilder("icon_xp", opponent.xp, "cooldown", opponent)
+      ]);
+    }
+    items.addAll([
+      const Expanded(child: SizedBox()),
+      SkinnedText(opponent.name),
+    ]);
     return Widgets.rect(
         alignment: Alignment.center,
-        padding: EdgeInsets.fromLTRB(24.d, 16.d, 24.d, 0),
+        padding: EdgeInsets.fromLTRB(24.d, 12.d, 24.d, 0),
         decoration: BoxDecoration(
             image: DecorationImage(
                 image:
@@ -111,20 +137,10 @@ class _LiveOutScreenState extends AbstractScreenState<LiveOutScreen> {
                 centerSlice: sliceData.centerSlice)),
         width: 400.d,
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            _titleBuilder(fraction, opponent),
-            SizedBox(height: 10.d),
-            _rewardItemBuilder("icon_seed", opponent.score, "power", opponent),
-            _rewardItemBuilder("icon_gold", opponent.gold, "gold", opponent),
-            _rewardItemBuilder("icon_xp", opponent.xp, "cooldown", opponent),
-            const Expanded(child: SizedBox()),
-            SkinnedText(opponent.name)
-          ],
-        ));
+            crossAxisAlignment: CrossAxisAlignment.center, children: items));
   }
 
-  _titleBuilder(String fraction, OpponentResult opponent) {
+  _titleBuilder(OpponentResult opponent) {
     return SizedBox(
         child: Row(mainAxisSize: MainAxisSize.min, children: [
       Widgets.rect(
@@ -132,19 +148,19 @@ class _LiveOutScreenState extends AbstractScreenState<LiveOutScreen> {
           padding: EdgeInsets.all(6.d),
           color: TColors.black.withOpacity(0.3),
           child: LoaderWidget(AssetType.image, "avatar_${Random().nextInt(10)}",
-              width: 100.d, height: 100.d, subFolder: "avatars")),
+              width: 92.d, height: 92.d, subFolder: "avatars")),
       SizedBox(width: 12.d),
       Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            _usedCards(fraction, opponent),
+            _usedCards(opponent),
             SkinnedText("ˢ${opponent.power.compact()}", style: TStyles.small)
           ])
     ]));
   }
 
-  Widget _usedCards(String fraction, OpponentResult opponent) {
+  Widget _usedCards(OpponentResult opponent) {
     var slots = _result.slots[opponent.id]!.value;
     return SizedBox(
         width: 128.d,
@@ -152,16 +168,16 @@ class _LiveOutScreenState extends AbstractScreenState<LiveOutScreen> {
         child: Stack(
           clipBehavior: Clip.none,
           children: [
-            _usedCard(fraction, slots[0], -0.15, 0, 2.d),
-            _usedCard(fraction, slots[1], -0.05, 16.d, 0),
-            _usedCard(fraction, slots[4], 0.05, 37.d, 0),
-            _usedCard(fraction, slots[2], 0.15, 60.d, 1.d),
-            _usedCard(fraction, slots[3], 0.25, 86.d, 5.d),
+            _usedCard(opponent.fraction, slots[0], -0.15, 0, 2.d),
+            _usedCard(opponent.fraction, slots[1], -0.05, 16.d, 0),
+            _usedCard(opponent.fraction, slots[4], 0.05, 37.d, 0),
+            _usedCard(opponent.fraction, slots[2], 0.15, 60.d, 1.d),
+            _usedCard(opponent.fraction, slots[3], 0.25, 86.d, 5.d),
           ],
         ));
   }
 
-  Widget _usedCard(String fraction, AccountCard? card, double angle,
+  Widget _usedCard(OpponentSide fraction, AccountCard? card, double angle,
       double left, double top) {
     return Positioned(
         top: top,
@@ -170,7 +186,7 @@ class _LiveOutScreenState extends AbstractScreenState<LiveOutScreen> {
         child: Transform.rotate(
             angle: angle,
             child: Asset.load<Image>(
-                "liveout_card_${card == null ? "missed" : fraction}")));
+                "liveout_card_${card == null ? "missed" : fraction.name}")));
   }
 
   Widget _rewardItemBuilder(
