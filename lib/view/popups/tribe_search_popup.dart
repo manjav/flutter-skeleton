3c08038d0ce886074b2d2ee6f52fd3b68eb41193
Mansour <mansurjavadi@gmail.com>
2023-09-28 14:59:15 +0300

    Join to searched tribes

--- lib/view/popups/tribe_search_popup.dart
+++ lib/view/popups/tribe_search_popup.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../blocs/account_bloc.dart';
 
 import '../../blocs/services_bloc.dart';
 import '../../data/core/building.dart';
@@ -44,7 +45,9 @@ class _TribeSearchPopupState extends AbstractPopupState<TribeSearchPopup> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Row(children: [
-          Expanded(child: Widgets.skinnedInput(controller: _inputController)),
+          Expanded(
+              child: Widgets.skinnedInput(
+                  controller: _inputController, onChanged: (t) => _search())),
           SizedBox(width: 20.d),
           Widgets.skinnedButton(
               label: "search_l".l(),
@@ -68,67 +71,85 @@ class _TribeSearchPopupState extends AbstractPopupState<TribeSearchPopup> {
   Widget? _listItemBuilder(BuildContext context, int index) {
     var tribe = _tribes[index];
     return Widgets.button(
-        margin: EdgeInsets.all(4.d),
-        padding: EdgeInsets.all(22.d),
-        decoration:
-            Widgets.imageDecore("tribe_item_bg", ImageCenterSliceData(56, 56)),
-        child: Column(
+      margin: EdgeInsets.all(4.d),
+      padding: EdgeInsets.all(22.d),
+      decoration:
+          Widgets.imageDecore("tribe_item_bg", ImageCenterSliceData(56, 56)),
+      child: Column(children: [
+        Row(
           children: [
-            Row(
-              children: [
-                Widgets.rect(
-                    radius: 20.d,
-                    color: TColors.primary10,
-                    padding: EdgeInsets.fromLTRB(12.d, 6.d, 12.d, 6.d),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Asset.load<Image>("icon_score", width: 50.d),
-                          SizedBox(width: 12.d),
-                          SkinnedText(tribe.weeklyRank.compact())
-                        ])),
-                SizedBox(width: 18.d),
-                Expanded(
-                    child: SkinnedText(tribe.name,
-                        alignment: Alignment.centerLeft)),
-                Widgets.rect(
-                    radius: 20.d,
-                    width: 210.d,
-                    color: TColors.primary10,
-                    padding: EdgeInsets.fromLTRB(12.d, 6.d, 12.d, 6.d),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Asset.load<Image>("icon_population", width: 50.d),
-                          SkinnedText(
-                              " ${tribe.population}/${tribe.getOption(Buildings.base.id)}")
-                        ]))
-              ],
-            ),
-            SizedBox(height: 12.d),
-            Row(
-              children: [
-                Expanded(child: Text(tribe.description)),
-                Widgets.skinnedButton(
+            Widgets.rect(
+                radius: 20.d,
+                color: TColors.primary10,
+                padding: EdgeInsets.fromLTRB(12.d, 6.d, 12.d, 6.d),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Asset.load<Image>("icon_score", width: 50.d),
+                      SizedBox(width: 12.d),
+                      SkinnedText(tribe.weeklyRank.compact())
+                    ])),
+            SizedBox(width: 18.d),
+            Expanded(
+                child:
+                    SkinnedText(tribe.name, alignment: Alignment.centerLeft)),
+            Widgets.rect(
+                radius: 20.d,
+                width: 210.d,
+                color: TColors.primary10,
+                padding: EdgeInsets.fromLTRB(12.d, 6.d, 12.d, 6.d),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Asset.load<Image>("icon_population", width: 50.d),
+                      SkinnedText(
+                          " ${tribe.population}/${tribe.getOption(Buildings.base.id)}")
+                    ]))
+          ],
+        ),
+        SizedBox(height: 12.d),
+        Row(
+          children: [
+            Expanded(
+                child: Text(tribe.description,
+                    style: TStyles.medium.copyWith(height: 1))),
+            IgnorePointer(
+                ignoring: true,
+                child: Widgets.skinnedButton(
                     height: 110.d,
                     width: 210.d,
                     padding: EdgeInsets.fromLTRB(28.d, 0, 28.d, 24.d),
                     color: ButtonColor.teal,
-                    label: "join_l".l())
-              ],
-            )
+                    label: "join_l".l()))
           ],
-        ));
+        )
+      ]),
+      onPressed: () => _join(tribe),
+    );
   }
 
   _search() async {
     try {
       var result = await BlocProvider.of<ServicesBloc>(context)
           .get<HttpConnection>()
-          .rpc(RpcId.tribeSearch,
+          .tryRpc(context, RpcId.tribeSearch,
               params: {RpcParams.query.name: _inputController.text});
       _tribes = Tribe.initAll(result["tribes"]);
       setState(() {});
+    } finally {}
+  }
+
+  _join(Tribe tribe) async {
+    try {
+      var result = await BlocProvider.of<ServicesBloc>(context)
+          .get<HttpConnection>()
+          .tryRpc(context, RpcId.tribeJoin,
+              params: {RpcParams.tribe_id.name: tribe.id});
+      if (mounted) {
+        var bloc = BlocProvider.of<AccountBloc>(context);
+        bloc.account!.map["tribe"] = Tribe(result["tribe"]);
+        bloc.add(SetAccount(account: bloc.account!));
+      }
     } finally {}
   }
 }
