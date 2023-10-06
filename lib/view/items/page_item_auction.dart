5c2efb42c0b8cb57c682368cd5f2dcfb1aee3956
Mansour <mansurjavadi@gmail.com>
2023-10-06 23:14:18 +0300

    Show `MyDeals` and `MySells` in auction

--- lib/view/items/page_item_auction.dart
+++ lib/view/items/page_item_auction.dart
@@ -57,18 +57,20 @@ class _AuctionPageItemState extends AbstractPageItemState<AbstractPageItem>
             _mainTabItemRenderer(1, "deals"),
             _mainTabItemRenderer(2, "sells"),
           ])),
-      Widgets.rect(
-          radius: 132.d,
-          height: 110.d,
-          color: TColors.black80,
-          child: Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                for (var i = 0; i < tabsName.length; i++)
-                  _tabItemRenderer(i, tabsName[i])
-              ])),
+      _selectedMainTab == 0
+          ? Widgets.rect(
+              radius: 132.d,
+              height: 110.d,
+              color: TColors.black80,
+              child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    for (var i = 0; i < tabsName.length; i++)
+                      _tabItemRenderer(i, tabsName[i])
+                  ]))
+          : const SizedBox(),
       SizedBox(height: 32.d),
       Expanded(
           child: GridView.builder(
@@ -98,6 +100,8 @@ class _AuctionPageItemState extends AbstractPageItemState<AbstractPageItem>
             .get<HttpConnection>()
             .tryRpc(
                 context, index == 1 ? RpcId.auctionDeals : RpcId.auctionSells);
+        _cards = AuctionCard.getList(_account, data).values.toList();
+        setState(() {});
       } finally {}
     }
   }
@@ -136,7 +140,7 @@ class _AuctionPageItemState extends AbstractPageItemState<AbstractPageItem>
           .get<HttpConnection>()
           .tryRpc(context, RpcId.auctionSearch, params: params);
       _selectedTab = index;
-      _cards = AuctionCard.getList(_account, data);
+      _cards = AuctionCard.getList(_account, data).values.toList();
       setState(() {});
     } finally {}
   }
@@ -144,63 +148,74 @@ class _AuctionPageItemState extends AbstractPageItemState<AbstractPageItem>
   Widget _cardItemBuilder(AuctionCard card, int secondsOffset) {
     var cardSize = 230.d;
     var radius = Radius.circular(36.d);
+    var bidable = card.activityStatus > 0 &&
+        (card.ownerId != _account.get(AccountField.id) &&
+            card.maxBidderId != _account.get(AccountField.id));
+    var time = card.activityStatus > 0
+        ? (card.createdAt + secondsOffset).toRemainingTime()
+        : "closed_l".l();
+    var bidderName = "auction_bid".l();
+    if (!bidable) {
+      bidderName +=
+          "\n${card.maxBidderId == _account.get(AccountField.id) ? "you_l".l() : card.maxBidderName}\n";
+    }
     return Widgets.button(
-      radius: radius.x,
-      padding: EdgeInsets.zero,
-      margin: EdgeInsets.all(8.d),
-      color: TColors.primary90,
-      child: Row(children: [
-        Widgets.rect(
-            width: cardSize + 16.d,
-            padding: EdgeInsets.all(8.d),
-            child: CardItem(card,
-                size: cardSize,
-                showCooldown: false,
-                key: getGlobalKey(card.id),
-                heroTag: "hero_${card.id}")),
-        Expanded(
-            child: Stack(
-          alignment: Alignment.bottomCenter,
-          children: [
-            Positioned(
-                top: 0,
-                right: 0,
-                width: 200.d,
-                height: 70.d,
-                child: Widgets.rect(
-                    borderRadius:
-                        BorderRadius.only(topRight: radius, bottomLeft: radius),
-                    color: TColors.primary70,
-                    child: SkinnedText(
-                        "ˣ${(card.createdAt + secondsOffset).toRemainingTime()}"))),
-            Positioned(
-                left: 0,
-                right: 8.d,
-                bottom: 8.d,
-                child: Column(mainAxisSize: MainAxisSize.min, children: [
-                  Text("auction_bid".l(),
-                      style: TStyles.medium.copyWith(height: 1)),
-                  Row(mainAxisSize: MainAxisSize.min, children: [
-                    Asset.load<Image>("icon_gold", width: 60.d),
-                    SizedBox(width: 8.d),
-                    SkinnedText(card.maxBid.compact(), style: TStyles.large)
-                  ]),
-                  SizedBox(height: 8.d),
-                  Widgets.skinnedButton(
-                      padding: EdgeInsets.fromLTRB(0, 12.d, 8.d, 32.d),
-                      color: ButtonColor.green,
-                      child: Row(mainAxisSize: MainAxisSize.min, children: [
-                        Asset.load<Image>("icon_gold", width: 60.d),
-                        SizedBox(width: 4.d),
-                        SkinnedText("+${card.bidStep.compact()}",
-                            style: TStyles.large)
-                      ])),
-                ])),
-          ],
-        )),
-      ]),
-      onPressed: () => _bid(card),
-    );
+        radius: radius.x,
+        padding: EdgeInsets.zero,
+        margin: EdgeInsets.all(8.d),
+        color: TColors.primary90,
+        child: Row(children: [
+          Widgets.rect(
+              width: cardSize + 16.d,
+              padding: EdgeInsets.all(8.d),
+              child: CardItem(card,
+                  size: cardSize,
+                  showCooldown: false,
+                  key: getGlobalKey(_selectedMainTab * 10000 + card.id),
+                  heroTag: "hero_${_selectedMainTab}_${card.id}")),
+          Expanded(
+              child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Positioned(
+                  top: 0,
+                  right: 0,
+                  width: 200.d,
+                  height: 70.d,
+                  child: Widgets.rect(
+                      borderRadius: BorderRadius.only(
+                          topRight: radius, bottomLeft: radius),
+                      color: TColors.primary70,
+                      child: SkinnedText("ˣ$time"))),
+              Positioned(
+                  left: 0,
+                  right: 8.d,
+                  bottom: 8.d,
+                  child: Column(mainAxisSize: MainAxisSize.min, children: [
+                    Text(bidderName, style: TStyles.medium.copyWith(height: 1)),
+                    Row(mainAxisSize: MainAxisSize.min, children: [
+                      Asset.load<Image>("icon_gold", width: 60.d),
+                      SizedBox(width: 8.d),
+                      SkinnedText(card.maxBid.compact(), style: TStyles.large)
+                    ]),
+                    SizedBox(height: 8.d),
+                    bidable
+                        ? Widgets.skinnedButton(
+                            padding: EdgeInsets.fromLTRB(0, 12.d, 8.d, 32.d),
+                            color: ButtonColor.green,
+                            child:
+                                Row(mainAxisSize: MainAxisSize.min, children: [
+                              Asset.load<Image>("icon_gold", width: 60.d),
+                              SizedBox(width: 4.d),
+                              SkinnedText("+${card.bidStep.compact()}",
+                                  style: TStyles.large)
+                            ]),
+                            onPressed: () => _bid(card))
+                        : const SizedBox(),
+                  ])),
+            ],
+          )),
+        ]));
   }
 
   _bid(AuctionCard card) async {
