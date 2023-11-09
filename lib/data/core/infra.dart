b25c8275b62f4c6f92d0dbe7b80d976a7c624221
Mansour <mansurjavadi@gmail.com>
2023-11-09 16:17:14 +0300

    Remove bundle class

--- lib/data/core/infra.dart
+++ lib/data/core/infra.dart
@@ -1,19 +1,3 @@
-class Bundle {
-  var outcomes = <String, int>{};
-  var incomes = <String, int>{};
-  Bundle([Map<String, int>? outcomes, Map<String, int>? incomes]) {
-    if (outcomes != null) this.outcomes = outcomes;
-    if (incomes != null) this.incomes = incomes;
-  }
-
-  static Bundle fromData(dynamic data) {
-    return Bundle(
-        Map.castFrom(data["outcomes"]), Map.castFrom(data['incomes']));
-  }
-}
-
-enum Bundles { search, shop_0 }
-
 class StringMap<T> {
   final map = <String, T>{};
   void initialize(Map<String, dynamic> data, {dynamic args}) {
