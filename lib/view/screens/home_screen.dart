7883f6e7b28029095a635ed6e58a9083c653be69
Mansour <mansurjavadi@gmail.com>
2023-12-19 20:12:27 +0300

    Move `TabNavigator` mechanism to dedicated class

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:rive/rive.dart';
-// ignore: implementation_imports
-import 'package:rive/src/rive_core/assets/file_asset.dart';
+import 'package:flutter_skeleton/data/core/adam.dart';
+import 'package:flutter_skeleton/data/core/rpc.dart';
+import 'package:flutter_skeleton/view/widgets/tab_navigator.dart';
 
 import '../../blocs/account_bloc.dart';
 import '../../blocs/services_bloc.dart';
-import '../../data/core/account.dart';
-import '../../data/core/building.dart';
 import '../../data/core/fruit.dart';
 import '../../data/core/infra.dart';
 import '../../mixins/key_provider.dart';
@@ -17,9 +14,7 @@ import '../../services/connection/noob_socket.dart';
 import '../../services/deviceinfo.dart';
 import '../../services/localization.dart';
 import '../../services/sounds.dart';
-import '../../services/theme.dart';
 import '../../utils/assets.dart';
-import '../../utils/utils.dart';
 import '../../view/items/page_item_auction.dart';
 import '../../view/items/page_item_tribe.dart';
 import '../items/page_item_cards.dart';
@@ -30,8 +25,6 @@ import '../route_provider.dart';
 import '../widgets.dart';
 import '../widgets/indicator.dart';
 import '../widgets/indicator_level.dart';
-import '../widgets/loaderwidget.dart';
-import '../widgets/skinnedtext.dart';
 import 'iscreen.dart';
 
 class HomeScreen extends AbstractScreen {
@@ -43,15 +36,14 @@ class HomeScreen extends AbstractScreen {
 
 class _HomeScreenState extends AbstractScreenState<AbstractScreen>
     with RewardScreenMixin, KeyProvider {
-  int _selectedTab = 2;
-  final double _navbarHeight = 210.d;
-  final _tabInputs = List<SMIBool?>.generate(5, (index) => null);
+  final int _tabsCont = 5;
   late PageController _pageController;
+  final ValueNotifier<int> _selectedTab = ValueNotifier(2);
 
   @override
   void initState() {
     waitingSFX = "";
-    _pageController = PageController(initialPage: _selectedTab);
+    _pageController = PageController(initialPage: _selectedTab.value);
     super.initState();
   }
 
@@ -70,7 +62,7 @@ class _HomeScreenState extends AbstractScreenState<AbstractScreen>
 
   @override
   List<Widget> appBarElementsLeft() {
-    if (_selectedTab != 2) return [];
+    if (_selectedTab.value != 2) return [];
     return [
       SizedBox(
         width: 196.d,
@@ -84,13 +76,13 @@ class _HomeScreenState extends AbstractScreenState<AbstractScreen>
 
   @override
   List<Widget> appBarElementsRight() {
-    if (_selectedTab == 3) {
+    if (_selectedTab.value == 3) {
       return [];
     }
-    if (_selectedTab == 4) {
+    if (_selectedTab.value == 4) {
       return [Indicator(widget.type.name, Values.gold)];
     }
-    if (_selectedTab == 2) {
+    if (_selectedTab.value == 2) {
       return <Widget>[
         ...super.appBarElementsRight()
           ..add(Widgets.button(
@@ -107,7 +99,6 @@ class _HomeScreenState extends AbstractScreenState<AbstractScreen>
 
   @override
   Widget contentFactory() {
-    var tabSize = DeviceInfo.size.width / _tabInputs.length;
     return BlocBuilder<AccountBloc, AccountState>(builder: (context, state) {
       return Stack(
         alignment: Alignment.bottomCenter,
@@ -115,18 +106,14 @@ class _HomeScreenState extends AbstractScreenState<AbstractScreen>
           backgrounBuilder(color: 2, animated: false),
           PageView.builder(
             controller: _pageController,
-            itemCount: _tabInputs.length,
+            itemCount: _tabsCont,
             itemBuilder: _pageItemBuilder,
             onPageChanged: (value) => _selectTap(value, pageChange: false),
           ),
-          SizedBox(
-              height: _navbarHeight,
-              child: ListView.builder(
-                  itemExtent: tabSize,
-                  itemBuilder: (c, i) =>
-                      _tabItemBuilder(state.account, i, tabSize),
-                  scrollDirection: Axis.horizontal,
-                  itemCount: _tabInputs.length)),
+          TabNavigator(
+              tabsCount: _tabsCont,
+              selectedIndex: _selectedTab,
+              onChange: (i) => _selectTap(i, tabsChange: false)),
           //   BlocConsumer<ServicesBloc, ServicesState>(
           //       builder: (context, state) => const SizedBox(),
           //       listener: (context, state) => _selectTap(state.data as int))
@@ -146,57 +133,11 @@ class _HomeScreenState extends AbstractScreenState<AbstractScreen>
     };
   }
 
-  Widget? _tabItemBuilder(Account account, int index, double size) {
-    return Widgets.touchable(
-        onTap: () => _selectTap(index, tabsChange: false),
-        child: Stack(
-          clipBehavior: Clip.none,
-          alignment: Alignment.center,
-          children: [
-            Positioned(
-                top: index == 3 ? 10.d : 0,
-                width: size * (index == 3 ? 0.6 : 1),
-                height: size * (index == 3 ? 0.6 : 1),
-                child: LoaderWidget(
-                  AssetType.animation,
-                  "tab_$index",
-                  fit: BoxFit.fitWidth,
-                  riveAssetLoader: _onTabAssetLoad,
-                  onRiveInit: (Artboard artboard) {
-                    final controller =
-                        StateMachineController.fromArtboard(artboard, "Tab");
-                    _tabInputs[index] =
-                        controller!.findInput<bool>("active") as SMIBool;
-                    _tabInputs[index]!.value =
-                        index == _pageController.initialPage;
-                    if (index == 3) {
-                      var input =
-                          controller.findInput<double>("level") as SMINumber;
-                      input.value = account.tribe != null
-                          ? account.tribe!.levels[Buildings.base.id]!.toDouble()
-                          : 0.0;
-                    }
-                    artboard.addController(controller);
-                  },
-                )),
-            _selectedTab == index
-                ? Positioned(
-                    bottom: 6.d,
-                    child: SkinnedText("home_tab_$index".l().toPascalCase(),
-                        style: TStyles.small))
-                : const SizedBox()
-          ],
-        ));
-  }
-
   _selectTap(int index, {bool tabsChange = true, bool pageChange = true}) {
     changeBackgroundColor(index + 1);
 
     if (tabsChange) {
-      for (var i = 0; i < _tabInputs.length; i++) {
-        _tabInputs[i]!.value = i == index;
-      }
-      setState(() => _selectedTab = index);
+      setState(() => _selectedTab.value = index);
     }
     if (pageChange) {
       _pageController.animateToPage(index,
@@ -225,7 +166,7 @@ class _HomeScreenState extends AbstractScreenState<AbstractScreen>
           }
         }
       });
-        return;
+      return;
     }
     if (message.type == Noobs.auctionBid) {
       var bid = message as NoobAuctionMessage;
@@ -254,15 +195,6 @@ class _HomeScreenState extends AbstractScreenState<AbstractScreen>
         bloc.add(SetAccount(account: bloc.account!));
       }
     }
-  }
-
-  Future<bool> _onTabAssetLoad(FileAsset asset, Uint8List? list) async {
-    if (asset is ImageAsset && asset.name == "background") {
-      var bytes = await rootBundle.load('assets/images/tab_background.webp');
-      asset.image = await ImageAsset.parseBytes(bytes.buffer.asUint8List());
-      return true;
-    }
-    return false;
   }
 
   @override
