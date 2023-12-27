762bc72ffced7f68328a0f3fcdd76caef3c21e41
Mansour <mansurjavadi@gmail.com>
2023-12-27 22:33:28 +0300

    Move levelup after outcomes

--- lib/view/screens/deck_screen.dart
+++ lib/view/screens/deck_screen.dart
@@ -291,11 +291,14 @@ class _DeckScreenState extends AbstractScreenState<DeckScreen>
         onClose: (data) async {
       _selectedCards.clear(setNull: true);
       if (mounted) {
-        accountBloc.add(SetAccount(account: account));
         var route =
             widget.opponent != null ? Routes.battleOut : Routes.questOut;
         if (data != null) {
           await Navigator.pushNamed(context, route.routeName, arguments: data);
+          if (mounted) {
+            accountBloc.account!.update(context, data);
+          }
+          accountBloc.add(SetAccount(account: account));
           if (mounted) Navigator.pop(context);
         }
       }
