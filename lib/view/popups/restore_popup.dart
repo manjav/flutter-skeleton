d1bf598b11f86b637e9feb289d6c264859a41234
Mansour <mansurjavadi@gmail.com>
2023-12-27 22:26:18 +0300

    Mini mode (only set) ability

--- lib/view/popups/restore_popup.dart
+++ lib/view/popups/restore_popup.dart
@@ -14,7 +14,8 @@ import '../widgets.dart';
 import '../widgets/skinned_text.dart';
 
 class RestorePopup extends AbstractPopup {
-  RestorePopup({super.key}) : super(Routes.popupRestore, args: {});
+  const RestorePopup({required super.args, super.key})
+      : super(Routes.popupRestore);
 
   @override
   createState() => _RestorePopupState();
@@ -22,16 +23,14 @@ class RestorePopup extends AbstractPopup {
 
 class _RestorePopupState extends AbstractPopupState<RestorePopup> {
   @override
-  BoxDecoration get chromeSkinBuilder =>
-      Widgets.imageDecorator("popup_chrome_pink", ImageCenterSliceData(410, 460));
+  BoxDecoration get chromeSkinBuilder => Widgets.imageDecorator(
+      "popup_chrome_pink", ImageCenterSliceData(410, 460));
 
-  late Account _account;
   late TextEditingController _textController;
 
   @override
   void initState() {
     _textController = TextEditingController();
-    _account = accountBloc.account!;
     super.initState();
   }
 
@@ -41,34 +40,35 @@ class _RestorePopupState extends AbstractPopupState<RestorePopup> {
   @override
   contentFactory() {
     var style = TStyles.medium.copyWith(height: 1);
+    var items = <Widget>[];
+    if (!widget.args.containsKey("onlySet")) {
+      items.addAll([
+        Text("settings_restore_get".l(), style: style),
+        SizedBox(height: 20.d),
+        SkinnedText("settings_restore_yours".l()),
+        Widgets.clipboardGetter(accountBloc.account!.restoreKey),
+        Widgets.divider(width: 120.d, margin: 32.d),
+      ]);
+    }
+    items.addAll([
+      Text("settings_restore_set".l(), style: style),
+      SizedBox(height: 20.d),
+      Widgets.skinnedInput(
+          maxLines: 1,
+          controller: _textController,
+          hintText: "settings_restore_hint".l(),
+          onChange: (t) => setState(() {})),
+      SizedBox(height: 30.d),
+      Widgets.skinnedButton(
+          width: 590.d,
+          color: ButtonColor.green,
+          icon: "icon_restore",
+          label: "settings_restore".l(),
+          isEnable: _textController.text.isNotEmpty,
+          onPressed: _restoreAccount)
+    ]);
     return SizedBox(
-        height: 960.d,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SizedBox(height: 30.d),
-            Text("settings_restore_get".l(), style: style),
-            SizedBox(height: 30.d),
-            SkinnedText("settings_restore_yours".l()),
-            Widgets.clipboardGetter(_account.restoreKey),
-            Widgets.divider(width: 120.d, margin: 36.d),
-            Text("settings_restore_set".l(), style: style),
-            SizedBox(height: 30.d),
-            Widgets.skinnedInput(
-                maxLines: 1,
-                controller: _textController,
-                hintText: "settings_restore_hint".l(),
-                onChange: (t) => setState(() {})),
-            SizedBox(height: 40.d),
-            Widgets.skinnedButton(
-                width: 590.d,
-                color: ButtonColor.green,
-                icon: "icon_restore",
-                label: "settings_restore".l(),
-                isEnable: _textController.text.isNotEmpty,
-                onPressed: _restoreAccount),
-          ],
-        ));
+        child: Column(mainAxisSize: MainAxisSize.min, children: items));
   }
 
   _restoreAccount() {
