efcfefaa4ca97948a21a8e87dcadde847735f692
Mansour <mansurjavadi@gmail.com>
2024-01-08 16:53:10 +0300

    Add confirm ability for message popup

--- lib/view/popups/message_popup.dart
+++ lib/view/popups/message_popup.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_skeleton/services/localization.dart';
+import 'package:flutter_skeleton/view/widgets.dart';
 
 import '../../services/device_info.dart';
 import '../../services/theme.dart';
-import 'popup.dart';
 import '../route_provider.dart';
+import 'popup.dart';
 
 class MessagePopup extends AbstractPopup {
   const MessagePopup({super.key, required super.args})
@@ -15,21 +17,34 @@ class MessagePopup extends AbstractPopup {
 
 class _MessagePopupState extends AbstractPopupState<MessagePopup> {
   @override
-  titleBuilder() => widget.args['title'];
+  titleBuilder() => widget.args["title"];
 
   @override
   contentFactory() {
-    return SizedBox(
-        height: 220.d,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          // mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(widget.args['message'], style: TStyles.medium),
-            // SizedBox(height: 48.d),
-            // Widgets.labeledButton(
-            //     label: "card_sell".l(), color: "green", width: 370.d),
-          ],
-        ));
+    var items = <Widget>[
+      SizedBox(height: 64.d),
+      Text(widget.args["message"], style: TStyles.medium),
+      SizedBox(height: 86.d),
+    ];
+    if (widget.args.containsKey("isConfirm")) {
+      items.addAll([
+        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          _button(ButtonColor.gray, "decline_l".l(), null),
+          _button(ButtonColor.green, "accept_l".l(), true),
+        ])
+      ]);
+    }
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: items);
+  }
+
+  _button(ButtonColor color, String label, dynamic result) {
+    return Widgets.skinnedButton(
+        width: 360.d,
+        label: label,
+        color: color,
+        onPressed: () => Navigator.pop(context, result));
   }
 }
