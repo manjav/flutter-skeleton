bf74ab24a8925985637cbd32998558ce26da6e3f
mdtavangar <md.tavangar79@gmail.com>
2024-01-16 15:00:20 +0330

    fix import screen in views

--- lib/skeleton/views/views.dart
+++ lib/skeleton/views/views.dart
@@ -1,5 +1,2 @@
 export 'widgets.dart';
-// export '../../view/overlays/overlays_skeleton/overlays.dart';
-// export '../../view/popups/popups_skeleton/popups.dart';
-export 'screens/screens.dart';
 export 'widgets/widgets.dart';
