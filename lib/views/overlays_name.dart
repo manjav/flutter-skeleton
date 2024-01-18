aa888f97766297d99f0a5d8d0fb6ddd1fee9150e
mdtavangar <md.tavangar79@gmail.com>
2024-01-18 16:51:02 +0330

    add overlaysName

--- lib/views/overlays_name.dart
+++ lib/views/overlays_name.dart
@@ -1,6 +1,17 @@
 class OverlaysName {
-  static const toast = "overlay_toast";
   static const loading = "overlay_loading";
   static const confirm = "overlay_confirm";
+  static const toast = "overlay_toast";
   static const waiting = "overlay_waiting";
+  static const chatOptions = "overlay_chat_options";
+  static const member = "overlay_member";
+  static const outcome = "overlay_outcome";
+  static const feastAttack = "overlay_feast_attack";
+  static const feastEvolve = "overlay_feast_evolve";
+  static const feastLevelUp = "overlay_feast_level_up";
+  static const feastEnhance = "overlay_feast_enhance";
+  static const feastOpenPack = "overlay_feast_open_pack";
+  static const feastPurchase = "overlay_feast_purchase";
+  static const feastUpgrade = "overlay_feast_upgrade";
+  static const feastUpgradeCard = "overlay_feast_upgrade_card";
 }
