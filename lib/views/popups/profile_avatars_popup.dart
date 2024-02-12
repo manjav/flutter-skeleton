72dd351f51c5429518a6e477047db46ec948762f
mdtavangar <md.tavangar79@gmail.com>
2024-02-12 11:49:44 +0330

    fix profile avatars

--- lib/views/popups/profile_avatars_popup.dart
+++ lib/views/popups/profile_avatars_popup.dart
@@ -28,31 +28,53 @@ class _ProfileAvatarsPopupState
       child: CustomScrollView(
         shrinkWrap: true,
         slivers: [
-          ..._renderSet(0, 21),
-          ..._renderSet(101, 109),
-          ..._renderSet(109, 223),
+          ..._renderSet(0, 21, 0),
+          ..._renderSet(101, 109, 1),
+          ..._renderSet(109, 223, 2),
         ],
       ),
     );
   }
 
-  List<Widget> _renderSet(int start, int end) {
+  List<Widget> _renderSet(int start, int end, int index) {
     var account = accountProvider.account;
     var res = <Widget>[];
     res.add(SliverList(
       delegate: SliverChildListDelegate.fixed(
         [
           SizedBox(height: start > 0 ? 20.d : 0),
-          SkinnedText("profile_mood_title".l()),
+          Row(
+            children: [
+              Expanded(
+                  child: Transform.flip(
+                      flipX: true,
+                      child: Asset.load<Image>(
+                        "ui_divider_h_2",
+                      ))),
+              LoaderWidget(AssetType.image, "avatar_set_$index",
+                  subFolder: "avatars", height: 110.d, width: 156.d),
+              SizedBox(
+                width: 8.d,
+              ),
+              SkinnedText("profile_set_${index + 1}".l()),
+              SizedBox(
+                width: 15.d,
+              ),
+              Expanded(child: Asset.load<Image>("ui_divider_h_2")),
+            ],
+          ),
           SizedBox(height: 20.d),
         ],
       ),
     ));
-    res.add(SliverGrid.builder(
-      itemCount: end - start,
-      gridDelegate:
-          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5),
-      itemBuilder: (c, i) => _avatarItemBuilder(account, start + i),
+    res.add(SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 40.d),
+      sliver: SliverGrid.builder(
+        itemCount: end - start,
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5),
+        itemBuilder: (c, i) => _avatarItemBuilder(account, start + i),
+      ),
     ));
     return res;
   }
@@ -62,7 +84,7 @@ class _ProfileAvatarsPopupState
     return Widgets.button(
       context,
       radius: 32.d,
-      color: account.avatarId == id ? TColors.green : TColors.primary80,
+      color: account.avatarId == id ? TColors.primary20 : TColors.primary80,
       margin: EdgeInsets.all(8.d),
       padding: EdgeInsets.all(16.d),
       child: LoaderWidget(AssetType.image, "avatar_$id", subFolder: "avatars"),
