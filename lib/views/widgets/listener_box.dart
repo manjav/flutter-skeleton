971595e23c889e8c4efff20ba2e401a35aeaf995
Mansour <mansurjavadi@gmail.com>
2024-02-19 20:55:01 +0300

    Add live spell check

--- lib/views/widgets/listener_box.dart
+++ lib/views/widgets/listener_box.dart
@@ -1,21 +1,71 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../../app_export.dart';
 
 class ListenerBox extends StatelessWidget {
-  final Narrator narrator;
-  const ListenerBox(this.narrator, {super.key});
+  final Chat pattern;
+  const ListenerBox(this.pattern, {super.key});
 
   @override
   Widget build(BuildContext context) {
+    var correctStyle = TStyles.medium
+        .copyWith(color: TColors.green, fontWeight: FontWeight.w900);
     var stt = serviceLocator<STT>();
     var size = 100.d;
     return ValueListenableBuilder(
       valueListenable: stt.state,
       builder: (context, value, child) {
-        return Column(
+        return Row(
           children: [
+            Widgets.button(
+              context,
+              radius: size,
+              width: size * 0.5,
+              height: size * 0.5,
+              color: TColors.primary10,
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(16.d),
+              child: StreamBuilder(
+                stream: serviceLocator<Sounds>()
+                    .getPlayer(pattern.value)
+                    .onPlayerStateChanged,
+                builder: (context, snapshot) => Asset.load<SvgPicture>(
+                    snapshot.data == PlayerState.playing ? "stop" : "play",
+                    width: size * 0.3),
+              ),
+              onPressed: () => serviceLocator<Speaker>()
+                  .play(pattern.value, narrator: pattern.narrator),
+            ),
+            Expanded(
+              child: ValueListenableBuilder(
+                  valueListenable: stt.recognizedWords,
+                  builder: (context, value, child) {
+                    return Column(
+                      children: [
+                        RichText(
+                            text: TextSpan(
+                          style: TStyles.medium,
+                          children: _getWords(value, correctStyle),
+                        )),
+                        SizedBox(height: 4.d),
+                        Text(pattern.nativeLanguage,
+                            style: TStyles.small
+                                .copyWith(color: TColors.primary40),
+                            textDirection:
+                                pattern.nativeLanguage.getDirection()),
+                        SizedBox(height: 6.d),
+                        stt.state.value == STTState.fail
+                            ? Text(value,
+                                style: TStyles.small
+                                    .copyWith(color: TColors.error),
+                                textDirection: value.getDirection())
+                            : const SizedBox(),
+                      ],
+                    );
+                  }),
+            ),
             Widgets.button(
               context,
               width: size,
@@ -49,12 +99,6 @@ class ListenerBox extends StatelessWidget {
               ),
               onPressed: stt.startListening,
             ),
-            ValueListenableBuilder(
-              valueListenable: stt.recognizedWords,
-              builder: (context, value, child) => Text(value,
-                  style: TStyles.medium.copyWith(color: _getIconColor())),
-            ),
-            SizedBox(height: 16.d),
           ],
         );
       },
@@ -69,5 +113,30 @@ class ListenerBox extends StatelessWidget {
       STTState.done => TColors.primary40,
       _ => TColors.primary60,
     };
+  }
+
+  List<TextSpan> _getWords(String value, TextStyle correctStyle) {
+    if (pattern.value == value) {
+      return [TextSpan(text: pattern.value, style: correctStyle)];
+    }
+    var index = pattern.value.toLowerCase().indexOf(value);
+    if (index > -1) {
+      var spans = <TextSpan>[];
+      if (index > 0) {
+        spans.add(TextSpan(text: pattern.value.substring(0, index)));
+      }
+      if (value.isNotEmpty) {
+        spans.add(TextSpan(
+            text: pattern.value.substring(index, index + value.length),
+            style: correctStyle));
+      }
+      if (index + value.length < pattern.value.length - 1) {
+        spans
+            .add(TextSpan(text: pattern.value.substring(index + value.length)));
+      }
+      return spans;
+    } else {
+      return [TextSpan(text: pattern.value)];
+    }
   }
 }
