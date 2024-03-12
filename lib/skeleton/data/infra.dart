b551420378a8f55421d98def9edccb4d2100a43a
Mansour <mansurjavadi@gmail.com>
2024-03-12 16:59:27 +0300

    Add list notifier

--- lib/skeleton/data/infra.dart
+++ lib/skeleton/data/infra.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class StringMap<T> {
   final map = <String, T>{};
   void initialize(Map<String, dynamic> data, {dynamic args}) {
@@ -12,9 +14,28 @@ class StringMap<T> {
   }
 }
 
-class IntVec2d {
+class IntVec2 {
   final int i, j;
-  IntVec2d(this.i, this.j);
+  IntVec2(this.i, this.j);
   @override
   String toString() => "$i, $j";
+}
+
+class ListValueNotifier<T> extends ValueNotifier<List<T>> {
+  ListValueNotifier(super.value);
+
+  void add(T item) {
+    value.add(item);
+    notifyListeners();
+  }
+
+  void remove(T item) {
+    value.remove(item);
+    notifyListeners();
+  }
+
+  void removeLast() {
+    value.removeLast();
+    notifyListeners();
+  }
 }
