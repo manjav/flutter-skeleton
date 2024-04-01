749a2985a9ba4f555b7a492c11e35c6460d5db91
Mansour <mansurjavadi@gmail.com>
2024-04-01 15:19:52 +0300

    Add select name stage for onboarding

--- lib/views/screens/screen_onboarding.dart
+++ lib/views/screens/screen_onboarding.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../../app_export.dart';
 
 class OnboardingScreen extends AbstractScreen {
   OnboardingScreen({super.key}) : super(Routes.onboarding);
+  static String nativeLanguage = "", targetLanguage = "";
 
   @override
   createState() => _ScreenState();
 }
 
 class _ScreenState extends AbstractScreenState<OnboardingScreen> {
-  String _nativeLanguage = "", _targetLanguage = "";
   final PageController _pageController = PageController();
   @override
   List<Widget> appBarElementsLeft() => [];
@@ -21,7 +22,7 @@ class _ScreenState extends AbstractScreenState<OnboardingScreen> {
     List<MapEntry> languages =
         NetConnector.configs["supportedLanguages"].entries.toList();
     return PageView.builder(
-      itemCount: 2,
+      itemCount: 3,
       controller: _pageController,
       physics: const NeverScrollableScrollPhysics(),
       itemBuilder: (context, index) =>
@@ -31,12 +32,19 @@ class _ScreenState extends AbstractScreenState<OnboardingScreen> {
 
   Future<void> _onFlagSelect(int index, String value) async {
     if (index == 0) {
+      OnboardingScreen.nativeLanguage = value;
       _nativeLanguage = value;
       _pageController.animateToPage(1,
           duration: const Duration(milliseconds: 400), curve: Curves.easeOut);
+    } else if (index == 1) {
+      OnboardingScreen.targetLanguage = value;
+      _pageController.animateToPage(2,
+          duration: const Duration(milliseconds: 400), curve: Curves.easeOut);
     } else {
-      await serviceLocator<AccountProvider>()
-          .update(nativeLanguage: _nativeLanguage, targetLanguage: code);
+      await serviceLocator<AccountProvider>().update(
+          nativeLanguage: OnboardingScreen.nativeLanguage,
+          targetLanguage: OnboardingScreen.targetLanguage,
+          displayName: value);
       if (mounted) {
         Navigator.pop(context);
       }
@@ -48,8 +56,7 @@ class LanguagePage extends StatefulWidget {
   final int index;
   final List<MapEntry> languages;
   final Function(int, String)? onChange;
-  const LanguagePage(this.index, this.languages,
-      {this.onChange, super.key});
+  const LanguagePage(this.index, this.languages, {this.onChange, super.key});
 
   @override
   State<LanguagePage> createState() => _LanguagePageState();
@@ -70,7 +77,9 @@ class _LanguagePageState extends State<LanguagePage> {
     return Widgets.rect(
       padding: EdgeInsets.fromLTRB(56.d, 56.d, 56.d, 0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        crossAxisAlignment: widget.index > 1
+            ? CrossAxisAlignment.center
+            : CrossAxisAlignment.stretch,
         children: [
           Padding(
             padding: EdgeInsets.all(8.d),
@@ -90,12 +99,24 @@ class _LanguagePageState extends State<LanguagePage> {
             ],
             onChange: _onSearchBoxChange,
           ),
-          Expanded(
-            child: ListView.builder(
-                padding: EdgeInsets.all(16.d),
-                itemCount: _flags.length,
-                itemBuilder: (context, index) => _languageItemBuilder(index)),
-          ),
+          widget.index < 2
+              ? Expanded(
+                  child: ListView.builder(
+                      padding: EdgeInsets.all(16.d),
+                      itemCount: _flags.length,
+                      itemBuilder: (context, index) =>
+                          _languageItemBuilder(index)),
+                )
+              : SkinnedButton(
+                  icon: "tick",
+                  width: 80.d,
+                  height: 60.d,
+                  cornerRadius: 44.d,
+                  padding: EdgeInsets.all(20.d),
+                  margin: EdgeInsets.only(top: 40.d),
+                  isEnable: _textInputController.text.length > 2,
+                  onPressed: () => _submit(_textInputController.text),
+                ),
         ],
       ),
     );
@@ -104,14 +125,14 @@ class _LanguagePageState extends State<LanguagePage> {
   Widget _languageItemBuilder(int index) {
     return Widgets.button(
       context,
-        height: 64.d,
-        child: Row(
-          children: [
-            Asset.load<SvgPicture>("flags/${_flags[index].key}"),
-            SizedBox(width: 48.d),
-            Text(_flags[index].value)
-          ],
-        ),
+      height: 64.d,
+      child: Row(
+        children: [
+          Asset.load<SvgPicture>("flags/${_flags[index].key}"),
+          SizedBox(width: 48.d),
+          Text(_flags[index].value)
+        ],
+      ),
       onPressed: () => _submit(_flags[index].key),
     );
   }
