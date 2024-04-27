51388dad17f90105e0a2fa096a4bca217226626f
md tavangar <md.tavangar79@gmail.com>
2024-04-27 17:45:46 +0330

    upgrade tutorial overlay
    add character size,bottom,ignore pointer and radius to overlay

--- lib/views/overlays/tutorial_overlay.dart
+++ lib/views/overlays/tutorial_overlay.dart
@@ -17,9 +17,14 @@ class TutorialOverlay extends AbstractOverlay {
   final DialogueSide dialogueSide;
   final int handQuarterTurns;
   final bool showFocus;
+  final Size? characterSize;
+  final double? bottom;
+  final ValueNotifier<bool> ignorePointer;
+  final double? radius;
 
   const TutorialOverlay({
     required this.center,
+    required this.ignorePointer,
     this.onTap,
     this.showBackground = true,
     this.showHand = true,
@@ -31,6 +36,9 @@ class TutorialOverlay extends AbstractOverlay {
     this.dialogueSide = DialogueSide.left,
     this.handQuarterTurns = 0,
     this.showFocus = false,
+    this.characterSize,
+    this.bottom,
+    this.radius,
     super.key,
   }) : super(route: newRoute ?? OverlaysName.tutorial);
 
@@ -54,52 +62,63 @@ class _TutorialOverlayState extends AbstractOverlayState<TutorialOverlay> {
         ? Offset(widget.handPosition!.alongSize(page).dx,
             widget.handPosition!.alongSize(page).dy)
         : const Offset(0, 0);
-    return GestureDetector(
-      onTap: () async {
-        show.value = false;
-        await Future.delayed(700.ms);
-        if (widget.onTap != null) widget.onTap!();
-      },
-      child: Container(
-        height: Get.height,
-        width: Get.width,
-        color: widget.showBackground ? TColors.black25 : TColors.transparent,
-        child: Stack(
-          fit: StackFit.expand,
-          children: [
-            widget.showFocus
-                ? Container(
-                    width: Get.width,
-                    height: Get.height,
-                    decoration: BoxDecoration(
-                      gradient: RadialGradient(
-                        center: widget.center,
-                        radius: 0.3,
-                        colors: <Color>[
-                          Colors.white.withOpacity(0.0),
-                          Colors.black54,
-                        ],
-                        stops: const <double>[0.4, 1.0],
-                      ),
-                    ),
-                  )
-                : const SizedBox(),
-            widget.showCharacter
-                ? TutorialCharecter(
-                    show: show,
-                    text: widget.text,
-                    dialogueSide: widget.dialogueSide,
-                    bottom: 400.d,
-                    charecterName: widget.characterName,
-                  )
-                : const SizedBox(),
-            widget.showHand
-                ? TutorialHand(
-                    target: offset,
-                    quarterTurns: widget.handQuarterTurns,
-                  )
-                : const SizedBox(),
-          ],
+    return ValueListenableBuilder(
+      valueListenable: widget.ignorePointer,
+      builder: (context, value, child) => IgnorePointer(
+        ignoring: value,
+        child: GestureDetector(
+          onTap: () async {
+            show.value = false;
+            await Future.delayed(700.ms);
+            if (widget.onTap != null) widget.onTap!();
+          },
+          child: Container(
+            height: Get.height,
+            width: Get.width,
+            color:
+                widget.showBackground ? TColors.black25 : TColors.transparent,
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                widget.showFocus
+                    ? Container(
+                        width: Get.width,
+                        height: Get.height,
+                        decoration: BoxDecoration(
+                          gradient: RadialGradient(
+                            center: widget.center,
+                            // focalRadius: 0.1,
+                            // focal: Alignment.center,
+                            // transform: const GradientRotation(pi / 4),
+                            radius: widget.radius ?? 0.6,
+                            colors: <Color>[
+                              TColors.black.withOpacity(0.0),
+                              TColors.black80,
+                            ],
+                            stops: const <double>[0.41, 0.46],
+                          ),
+                        ),
+                      )
+                    : const SizedBox(),
+                widget.showCharacter
+                    ? TutorialCharacter(
+                        show: show,
+                        text: widget.text,
+                        dialogueSide: widget.dialogueSide,
+                        bottom: widget.bottom ?? 400.d,
+                        characterName: widget.characterName,
+                        characterSize: widget.characterSize,
+                      )
+                    : const SizedBox(),
+                widget.showHand
+                    ? TutorialHand(
+                        target: offset,
+                        quarterTurns: widget.handQuarterTurns,
+                      )
+                    : const SizedBox(),
+              ],
+            ),
+          ),
         ),
       ),
     );
