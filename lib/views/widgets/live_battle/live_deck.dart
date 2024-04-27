b5291da912d1aa4fd8f6de9a931407db4d8f9011
md tavangar <md.tavangar79@gmail.com>
2024-04-27 18:15:48 +0330

    update live deck for tutorial

--- lib/views/widgets/live_battle/live_deck.dart
+++ lib/views/widgets/live_battle/live_deck.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import '../../../app_export.dart';
 
@@ -20,61 +22,108 @@ class LiveDeck extends StatelessWidget with KeyProvider {
       right: 0,
       height: size / CardItem.aspectRatio,
       child: ValueListenableBuilder<List<AccountCard?>>(
-          valueListenable: items,
-          builder: (context, value, child) {
-            return PageView.builder(
-                reverse: Localization.isRTL,
-                clipBehavior: Clip.none,
-                onPageChanged: (index) => onFocus(index, items.value[index]!),
-                controller: pageController,
-                itemCount: items.value.length,
-                itemBuilder: (BuildContext context, int index) =>
-                    _cardBuilder(context, index, size));
-          }),
+        valueListenable: items,
+        builder: (context, value, child) {
+          return Row(
+            children: [
+              Expanded(
+                child: Container(
+                  color: TColors.black25,
+                  padding: EdgeInsets.symmetric(vertical: 20.d),
+                  child: PageView.builder(
+                    clipBehavior: Clip.hardEdge,
+                    onPageChanged: (index) =>
+                        onFocus(index, items.value[index]!),
+                    controller: pageController,
+                    itemCount: items.value.length,
+                    itemBuilder: (BuildContext context, int index) =>
+                        _cardBuilder(context, index, size),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 25.d,
+              ),
+              Widgets.touchable(
+                context,
+                // onTap: () => _onCardTap(context, index, item),
+                child: Widgets.rect(
+                  height: 180.d,
+                  width: 180.d,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Asset.load<Image>("button_battle_back",
+                          height: 180.d, width: 180.d),
+                      Asset.load<Image>("button_battle",
+                          height: 160.d, width: 160.d),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 25.d,
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 
   _cardBuilder(BuildContext context, int index, double size) {
-    var delta = 1.0,
-        normal = 1.0,
-        angle = 0.0,
-        scale = 1.0,
-        deltaX = 0.0,
-        deltaY = 0.0;
-    return AnimatedBuilder(
-        animation: pageController,
-        builder: (context, child) {
-          if (pageController.position.haveDimensions) {
-            delta = (pageController.page! - index);
-            angle = delta / (Localization.isRTL ? 10 : -10);
-            normal = (delta.abs() / 3).clamp(0, 1);
-            scale = 1 - Curves.easeInCirc.transform(normal);
-            deltaX = Curves.easeInSine.transform(normal) *
-                (delta > 0 ? 190.d : -190.d);
-            deltaY = Curves.easeInExpo.transform(normal) * 820.d;
-          }
-          var item = items.value[index]!;
-          return Transform.translate(
-            offset: Offset(deltaX, deltaY),
-            child: Transform.rotate(
-              angle: angle,
-              child: Transform.scale(
-                scale: scale,
-                child: Opacity(
-                  opacity: 1 - normal,
-                  child: Widgets.button(context,
-                      padding: EdgeInsets.zero,
-                      onPressed: () => _onCardTap(context, index, item),
-                      child: CardItem(item,
-                          size: size,
-                          key: getGlobalKey(item.id),
-                          showCooldown: false,
-                          showCoolOff: true)),
-                ),
-              ),
-            ),
-          );
-        });
+    var item = items.value[index]!;
+    return Widgets.button(
+      context,
+      padding: EdgeInsets.zero,
+      margin: EdgeInsets.only(right: 10.d),
+      onPressed: () => _onCardTap(context, index, item),
+      child: CardItem(item,
+          size: size,
+          key: getGlobalKey(item.id),
+          showCooldown: false,
+          showCoolOff: true),
+    );
+    // var delta = 1.0,
+    //     normal = 1.0,
+    //     angle = 0.0,
+    //     scale = 1.0,
+    //     deltaX = 0.0,
+    //     deltaY = 0.0;
+    // return AnimatedBuilder(
+    //     animation: pageController,
+    //     builder: (context, child) {
+    //       if (pageController.position.haveDimensions) {
+    //         delta = (pageController.page! - index);
+    //         angle = delta / (Localization.isRTL ? 10 : -10);
+    //         normal = (delta.abs() / 3).clamp(0, 1);
+    //         scale = 1 - Curves.easeInCirc.transform(normal);
+    //         deltaX = Curves.easeInSine.transform(normal) *
+    //             (delta > 0 ? 190.d : -190.d);
+    //         deltaY = Curves.easeInExpo.transform(normal) * 820.d;
+    //       }
+    //       var item = items.value[index]!;
+    //       return Transform.translate(
+    //         offset: Offset(deltaX, deltaY),
+    //         child: Transform.rotate(
+    //           angle: angle,
+    //           child: Transform.scale(
+    //             scale: scale,
+    //             child: Opacity(
+    //               opacity: 1 - normal,
+    //               child: Widgets.button(context,
+    //                   padding: EdgeInsets.zero,
+    //                   onPressed: () => _onCardTap(context, index, item),
+    //                   child: CardItem(item,
+    //                       size: size,
+    //                       key: getGlobalKey(item.id),
+    //                       showCooldown: false,
+    //                       showCoolOff: true)),
+    //             ),
+    //           ),
+    //         ),
+    //       );
+    //     });
   }
 
   _onCardTap(BuildContext context, int index, AccountCard item) {
