44fd2d13d41cbbd3436a5cf7ab3678a80adffa49
md tavangar <md.tavangar79@gmail.com>
2024-05-14 09:45:33 +0330

    disable gift,chance box and bundle for now

--- lib/views/items/page_item_map.dart
+++ lib/views/items/page_item_map.dart
@@ -222,22 +222,24 @@ class _MainMapItemState extends AbstractPageItemState<MainMapPageItem> {
                   }),
             )
           : const SizedBox(),
-      PositionedDirectional(
-        bottom: 210.d,
-        end: 150.d,
-        height: 150.d,
-        child: _box(0, "06:12:06".l()),
-      ),
-      PositionedDirectional(
-          bottom: 210.d,
-          end: 330.d,
-          height: 150.d,
-          child: _box(1, "chance_box".l())),
-      PositionedDirectional(
-          bottom: 210.d,
-          end: 510.d,
-          height: 150.d,
-          child: _box(2, "gift_reward".l())),
+
+      /// Disable for now
+      // PositionedDirectional(
+      //   bottom: 210.d,
+      //   end: 150.d,
+      //   height: 150.d,
+      //   child: _box(0, "06:12:06".l()),
+      // ),
+      // PositionedDirectional(
+      //     bottom: 210.d,
+      //     end: 330.d,
+      //     height: 150.d,
+      //     child: _box(1, "chance_box".l())),
+      // PositionedDirectional(
+      //     bottom: 210.d,
+      //     end: 510.d,
+      //     height: 150.d,
+      //     child: _box(2, "gift_reward".l())),
       _building(account, Buildings.defense),
       _building(account, Buildings.offense),
       _building(account, Buildings.base),
