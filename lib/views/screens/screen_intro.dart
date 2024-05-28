19c7c332d7b9c382be8b448ad574e47cfe1478ce
md tavangar <md.tavangar79@gmail.com>
2024-05-28 20:16:16 +0330

    add intro feast to intro page

--- lib/views/screens/screen_intro.dart
+++ lib/views/screens/screen_intro.dart
@@ -116,20 +116,101 @@ class _IntroScreenState extends AbstractScreenState<IntroScreen> {
                 width: 341.d,
               ),
             );
-          })
+          }),
+          TutorialCharacter(
+            show: steps[0],
+            text: "olive_says_that_we're_under_attack",
+            dialogueSide: DialogueSide.right,
+            characterName: "character_olive",
+            bottom: 400.d,
+          ),
+          TutorialCharacter(
+            show: steps[1],
+            text: "sarhang_is_suprised",
+            dialogueSide: DialogueSide.left,
+            characterName: "character_sarhang",
+            bottom: 400.d,
+          ),
+          TutorialCharacter(
+            show: steps[2],
+            text: "porteghula_enters",
+            dialogueSide: DialogueSide.top,
+            characterName: "character_porteghula",
+            bottom: 400.d,
+            dialogueHeight: 350.d,
+            characterSize: Size(666.d, 859.d),
+          ),
+          TutorialCharacter(
+            show: steps[3],
+            text: "sarhang_asks_player_for_help",
+            dialogueSide: DialogueSide.left,
+            characterName: "character_sarhang",
+            bottom: 400.d,
+          ),
+          ValueListenableBuilder(
+            valueListenable: enableTouch,
+            builder: (context, value, child) {
+              if (!value) return const SizedBox();
+              return Widgets.touchable(
+                context,
+                onTap: () async {
+                  if (index == -1) return;
+                  steps[index++].value = false;
+                  if (index == 4) {
+                    await accountProvider.updateTutorial(context, 1, 10);
+                    Get.offNamed(Routes.deck);
+                    return;
+                  }
+                  serviceLocator<AccountProvider>().update(
+                      context, {"tutorial_index": index, "tutorial_id": index});
+                  await Future.delayed(700.ms);
+                  steps[index].value = true;
+                },
+              );
+            },
+          ),
+          Consumer<AccountProvider>(builder: (context, account, child) {
+            if (account.account.tutorial_id < 1) return const SizedBox();
+            return Positioned(
+              top: 82.d,
+              left: 59.d,
+              child: AnimatedOpacity(
+                duration: 200.ms,
+                opacity: account.account.tutorial_id == 0 ? 0.0 : 1.0,
+                child: SkinnedButton(
+                  label: "already_have_a_village".l(),
+                  width: 400.d,
+                  onPressed: () async {
+                    serviceLocator<RouteService>()
+                        .to(Routes.popupRestore, args: {"onlySet": true});
+                  },
+                  color: ButtonColor.violet,
+                ),
+              ),
+            );
+          }),
         ],
       ),
     );
   }
 
   onSelectLanguage(Locale locale) async {
-    await Get.updateLocale(locale);
+    Overlays.insert(
+      context,
+      IntroFeastOverlay(
+        onClose: (data) {
+          if (context.mounted) {
+            serviceLocator<AccountProvider>()
+                .update(context, {"tutorial_index": 1, "tutorial_id": 1});
+            index = 0;
+            enableTouch.value = true;
+            steps[0].value = true;
+          }
+        },
+      ),
+    );
     Pref.language.setString(locale.languageCode);
     await serviceLocator<Localization>().changeLocal(locale);
-    if (context.mounted) {
-      serviceLocator<AccountProvider>()
-          .update(context, {"tutorial_index": 1, "tutorial_id": 1});
-      checkTutorial();
-    }
+    await Get.updateLocale(locale);
   }
 }
