77f884f4fed208afd29730d1f70be96322fb435f
Mansour <mansurjavadi@gmail.com>
2024-06-03 13:13:43 +0300

    cleanups -  remove old screen

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -98,7 +98,6 @@ class _MyAppState extends State<MyApp>
             // _getPage(Routes.word, LessonWordScreen()),
             // _getPage(Routes.match, LessonMatchScreen()),
             // _getPage(Routes.dictation, LessonDictationScreen()),
-            _getPage(Routes.chat, LessonChatScreen()),
             _getPage(Routes.intro, LessonIntroScreen()),
             _getPage(Routes.onboarding, OnboardingScreen()),
             _getPage(Routes.popupMentor, const MentorPopup(), false),
--- lib/views/screens/export.dart
+++ lib/views/screens/export.dart
@@ -1,3 +1,2 @@
-export 'screen_lesson_chat.dart';
 export 'screen_lesson_intro.dart';
 export 'screen_onboarding.dart';
