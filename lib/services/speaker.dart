ac457c4a654b90bf3557e9aade03fbcea33175f5
Mansour <mansurjavadi@gmail.com>
2024-06-06 15:54:29 +0300

    enhance - add try for player

--- lib/services/speaker.dart
+++ lib/services/speaker.dart
@@ -12,32 +12,33 @@ class Speaker extends IService {
     Narrator narrator = Narrator.onyx,
     Function()? onComplete,
   }) async {
-    serviceLocator<STT>().stop();
-    // serviceLocator<Sounds>().stopAll();
-    var player = serviceLocator<Sounds>().getPlayer(text);
-    if (force) {
-      player.stop();
-    } else if (player.state == PlayerState.playing) {
-      player.stop();
-      return;
-    }
-    player.setPlaybackRate(narrator == Narrator.onyx ? 1.1 : 0.8);
-    if (onComplete != null) {
-      player.eventStream.listen((event) {
-        if (event.eventType == AudioEventType.complete) {
-          onComplete();
-        }
-      });
-    }
-    await player.play(
-      UrlSource(
-          //narrator == Narrator.onyx?
-          // "https://s1.matnyaar.ir/gpt/tts-ms.php?locale=fa-IR&voice=fa-IR-FaridNeural&input=$text${reset ? "&nocache" : ""}":
-          "https://s1.matnyaar.ir/gpt/tts.php?voice=${narrator.name}&input=$text${reset ? "&nocache" : ""}"),
-    );
-
-    await Future.doWhile(() => Future.delayed(const Duration(milliseconds: 50))
-        .then((_) => player.state != PlayerState.completed));
-        
+    try {
+      serviceLocator<STT>().stop();
+      // serviceLocator<Sounds>().stopAll();
+      var player = serviceLocator<Sounds>().getPlayer(text);
+      if (force) {
+        player.stop();
+      } else if (player.state == PlayerState.playing) {
+        player.stop();
+        return;
+      }
+      player.setPlaybackRate(narrator == Narrator.onyx ? 1.1 : 0.8);
+      if (onComplete != null) {
+        player.eventStream.listen((event) {
+          if (event.eventType == AudioEventType.complete) {
+            onComplete();
+          }
+        });
+      }
+      await player.play(
+        UrlSource(
+            //narrator == Narrator.onyx?
+            // "https://s1.matnyaar.ir/gpt/tts-ms.php?locale=fa-IR&voice=fa-IR-FaridNeural&input=$text${reset ? "&nocache" : ""}":
+            "https://s1.matnyaar.ir/gpt/tts.php?voice=${narrator.name}&input=$text${reset ? "&nocache" : ""}"),
+      );
+      await Future.doWhile(() =>
+          Future.delayed(const Duration(milliseconds: 50))
+              .then((_) => player.state != PlayerState.completed));
+    } finally {}
   }
 }
