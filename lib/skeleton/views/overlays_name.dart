73c7b4a77312015eb48e2324fdcd3b16bcf415f7
Mohamad Tavangar <md.tavangar79@gmail.com>
2024-06-08 09:56:42 +0330

    create feast chancebox

--- lib/skeleton/views/overlays_name.dart
+++ lib/skeleton/views/overlays_name.dart
@@ -18,6 +18,7 @@ class OverlaysName {
   static const feastDailyReward = "overlay_feast_daily_reward";
   static const feastBundle = "overlay_feast_bundle";
   static const feastGiftReward = "overlay_feast_gift_reward";
+  static const feastChanceBox = "overlay_feast_chance_box";
   static const tutorial = "overlay_tutorial";
   static const mission = "overlay_mission";
   static const helpMap = "overlay_help_map";
