fdf7dcffb63aabc0d00c6daf51c4fac889198b9b
Mansour <mansurjavadi@gmail.com>
2024-07-08 17:48:10 +0300

    fixbugs - empty space same color

--- lib/views/widgets/listener_box.dart
+++ lib/views/widgets/listener_box.dart
@@ -159,7 +159,7 @@ class ListenerBox extends StatelessWidget {
                   return Widgets.button(
                     context,
                     radius: 6.d,
-                    color: isHidden ? TColors.primary10 : TColors.transparent,
+                    color: isHidden ? TColors.primary0 : TColors.transparent,
                     margin: EdgeInsets.all(2.d),
                     padding: EdgeInsets.fromLTRB(4.d, 4.d, 4.d, 1.d),
                     child: Text(
