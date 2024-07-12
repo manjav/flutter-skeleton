87942322d62abec71eca110110a166f9df8b9ba3
Mansour <mansurjavadi@gmail.com>
2024-07-12 19:38:08 +0300

    enhance - screen wake lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:wakelock_plus/wakelock_plus.dart';
 
 import 'app_export.dart';
 
 void main() async {
   MyApp.startTime = DateTime.now();
   WidgetsFlutterBinding.ensureInitialized();
+  WakelockPlus.enable();
   // await Firebase.initializeApp();
   await Prefs().initialize();
 
   initServices();
-
   runApp(const MyApp());
 }
