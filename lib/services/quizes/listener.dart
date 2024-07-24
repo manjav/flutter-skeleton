e21057614fde057c7037fd2cb21e8c939bcfee0a
Mansour <mansurjavadi@gmail.com>
2024-07-24 13:18:31 +0300

    enhance - less accuracy

--- lib/services/quizes/listener.dart
+++ lib/services/quizes/listener.dart
@@ -87,7 +87,7 @@ class ListenerQuiz extends Quiz {
     required Narrator narrator,
     required String repeatVoice,
     String? locale,
-    int minMatchLevel = 92,
+    int minMatchLevel = 93,
     List<String>? exceptions,
     bool shouldPlayHint = true,
     Function(QuizState p1, String p2)? onResult,
