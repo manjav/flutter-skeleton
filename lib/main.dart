389362e8118499c3e3ea647ba8e6be0900379be2
Mansour <mansurjavadi@gmail.com>
2024-08-09 16:36:45 +0300

    cleanups

--- lib/main.dart
+++ lib/main.dart
@@ -29,22 +29,16 @@ void _catchErrors(bool fatalError) {
   // Non-async exceptions
   FlutterError.onError = (errorDetails) {
     if (fatalError) {
-      // If you want to record a "fatal" exception
       FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
-      // ignore: dead_code
     } else {
-      // If you want to record a "non-fatal" exception
       FirebaseCrashlytics.instance.recordFlutterError(errorDetails);
     }
   };
   // Async exceptions
   PlatformDispatcher.instance.onError = (error, stack) {
     if (fatalError) {
-      // If you want to record a "fatal" exception
       FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
-      // ignore: dead_code
     } else {
-      // If you want to record a "non-fatal" exception
       FirebaseCrashlytics.instance.recordError(error, stack);
     }
     return true;
