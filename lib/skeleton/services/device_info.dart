0d3a0ae8e8ee465ed613619daf72f11ef153e4b7
Mansour <mansurjavadi@gmail.com>
2024-08-14 16:38:11 +0300

    enhance - difactorsable scale ratio

--- lib/skeleton/services/device_info.dart
+++ lib/skeleton/services/device_info.dart
@@ -33,11 +33,11 @@ class DeviceInfo extends IService {
 
     // Get screen info
     var q = MediaQuery.of(context);
-    DeviceInfo.size = q.size;
-    DeviceInfo.devicePixelRatio = q.devicePixelRatio;
+    size = q.size;
+    devicePixelRatio = q.devicePixelRatio;
     var width = math.min(size.width, size.height);
     var height = math.max(size.width, size.height);
-    ratio = width / 390; // Comes from figma design
+    ratio = 1; //width / 390; // Comes from figma design
     aspectRatio = width / height;
 
     // Find advertise id
