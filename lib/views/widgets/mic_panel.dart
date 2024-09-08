f85faf2d009799b491ccfc27034d3f63f18252cf
Mansour <mansurjavadi@gmail.com>
2024-09-09 01:02:33 +0300

    enhance - debug mode ***

--- lib/views/widgets/mic_panel.dart
+++ lib/views/widgets/mic_panel.dart
@@ -136,13 +136,15 @@ class _MicPanelState extends State<MicPanel> {
     return ValueListenableBuilder(
       valueListenable: _state,
       builder: (context, value, child) {
-        return value == QuizState.failure
-            ? DirText(
+        return
+            // value == QuizState.failure ?
+            DirText(
                 _recognizedWords.value,
                 style: TStyles.medium.copyWith(color: TColors.error, height: 1),
                 textAlign: TextAlign.center,
               )
-            : SizedBox(height: 12.d);
+            // : SizedBox(height: 12.d)
+            ;
       },
     );
   }
