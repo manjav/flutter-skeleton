354316156c2cc923a20a19de0892ec19c1a8b456
Mansour <mansurjavadi@gmail.com>
2024-09-18 12:43:09 +0300

    feature - assets url in configs

--- lib/services/net_connector.dart
+++ lib/services/net_connector.dart
@@ -60,15 +60,17 @@ class NetConnector extends IService {
         }
         versions[key] = VersionConfigs(
           key,
-          entry.value["port"],
-          entry.value["host"],
-          entry.value["changelog"],
-          VersionPriority.values[entry.value["priority"]],
+          entry.value["port"] ?? configs["port"],
+          entry.value["host"] ?? configs["host"],
+          entry.value["assetsUrl"] ?? configs["assetsUrl"],
+          entry.value["changelog"] ?? "",
+          VersionPriority.values[entry.value["priority"] ?? 2],
         );
         // Update connection according to version
         if (version == key) {
           configs["port"] = versions[key]!.port;
           configs["host"] = versions[key]!.host;
+          configs["assetsUrl"] = versions[key]!.assetsUrl;
         }
       }
 
@@ -87,7 +89,7 @@ class NetConnector extends IService {
         }
       }
 
-      LoaderWidget.baseURL = configs["assetsServer"]!;
+      LoaderWidget.baseURL = configs["assetsUrl"]!;
       LoaderWidget.hashMap = Map.castFrom(configs["files"]);
       log("Config loaded.");
     } else {
@@ -275,12 +277,14 @@ class VersionConfigs {
   final int port;
   final int version;
   final String host;
+  final String assetsUrl;
   final String changelog;
   final VersionPriority priority;
   VersionConfigs(
     this.version,
     this.port,
     this.host,
+    this.assetsUrl,
     this.changelog,
     this.priority,
   );
