aaa58ecbbb7d8a2f40e54116cb3a9aea28505644
Mansour <mansurjavadi@gmail.com>
2024-10-30 11:48:13 +0300

    feature - captions

--- lib/views/screens/screen_imitation.dart
+++ lib/views/screens/screen_imitation.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 import '../../app_export.dart';
@@ -17,14 +18,102 @@ class _ScreenState extends AbstractScreenState<ImitationScreen>
   final ValueNotifier<VideoIntry?> _videoData = ValueNotifier(null);
   final List<VideoIntry> _captions = [];
   final ValueNotifier<bool> _captionMode = ValueNotifier(false);
+  final PageController _pageController = PageController();
+
   @override
   void initState() {
+    _pageController.addListener(
+      () => controller.slideIndex.value = (_pageController.page ?? 0).round(),
+    );
+    controller.slideIndex.addListener(_onSlideChange);
     initializeController();
     super.initState();
   }
 
   void _onSlideChange() {
     _playYoutube(controller.currentSlide);
+  }
+
+  void _nextSlide() => _pageController.nextPage(
+      duration: Duration(milliseconds: 300), curve: Curves.easeInOut);
+
+  @override
+  Widget contentBuilder(double paddingTop) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        SizedBox(height: 100.d),
+        ValueListenableBuilder(
+          valueListenable: _videoData,
+          builder: (context, value, child) {
+            return value == null ? SizedBox() : youtubePlayer();
+          },
+        ),
+        SizedBox(
+          height: 450.d,
+          child: PageView.builder(
+            controller: _pageController,
+            itemCount: controller.currentSerie.children.length,
+            itemBuilder: (context, index) => _slideRenderer(
+              controller.currentSerie.children[index] as ParentContent,
+            ),
+          ),
+        ),
+        SizedBox(height: 100.d),
+      ],
+    );
+  }
+
+  Widget _slideRenderer(ParentContent slide) {
+    final first = slide.children.first as Talk;
+    return Container(
+      margin: EdgeInsets.all(30.d),
+      padding: EdgeInsets.all(30.d),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: first.type == ContentType.station
+              ? TColors.transparent
+              : TColors.primary0,
+          border: Border.all(color: TColors.primary20, width: 2.d),
+          borderRadius: BorderRadius.all(Radius.circular(20.d))),
+        _captionSlideBuilder()
+      },
+    );
+  }
+
+  }
+
+  Widget _captionSlideBuilder() {
+    return ValueListenableBuilder(
+      valueListenable: _captionMode,
+      builder: (context, value, child) {
+        return Column(
+          children: [
+            _captionBuilder(value),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _captionBuilder(bool captionMode) {
+    return Expanded(
+      child: ValueListenableBuilder(
+        valueListenable: caption,
+        builder: (context, value, child) {
+          if (captionMode) {
+            if (value == null) return SizedBox();
+            return Text(
+              value.targetValue.simplify(),
+              // textAlign: TextAlign.center,
+              style: TStyles.big,
+            );
+          } else {
+            return LoaderWidget(AssetType.vector, "ear");
+          }
+        },
+      ),
+    );
   }
 
   Future<void> _playYoutube(ParentContent slide) async {
