9441a90a937f1ec9e10d117f507342c9b6ccdef6
Mansour <mansurjavadi@gmail.com>
2024-11-22 21:41:37 +0300

    fixbugs - remove unnecessary listeners

--- lib/skeleton/services/media_service.dart
+++ lib/skeleton/services/media_service.dart
@@ -95,10 +95,10 @@ class MediaService extends IService {
         mimeType: "audio/mpeg",
       );
       player.play(_sounds[name] = source);
-      player.onPlayerStateChanged
-          .listen((state) => entry!._parseState(state.name));
-      player.onPositionChanged
-          .listen((position) => entry!._setPosition(position));
+      // player.onPlayerStateChanged
+      //     .listen((state) => entry!._parseState(state.name));
+      // player.onPositionChanged
+      //     .listen((position) => entry!._setPosition(position));
       // player.play(_sounds[name] = DeviceFileSource(file!.path));
       // player.play(UrlSource('${LoaderWidget.baseURL}/sounds/$name.$extension'));
     } else {
