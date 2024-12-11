d9fcb6a1aed0f901296da047d0b15d90fa53e248
Mansour <mansurjavadi@gmail.com>
2024-12-11 22:22:51 +0300

    feature - profile: avatar and name

--- lib/views/items/home_item_profile.dart
+++ lib/views/items/home_item_profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import '../../app_export.dart';
 
@@ -12,8 +13,35 @@ class ProfilePageItem extends AbstractHomePageItem {
 class _ProfilePageItemState extends AbstractHomePageItemState<ProfilePageItem> {
   @override
   Widget build(BuildContext context) {
+    final account = serviceLocator<AccountProvider>();
     return CustomScrollView(
-      slivers: [sliverAppBar()],
+      slivers: [
+        sliverAppBar(),
+        _idBuilder(account),
+
+  Widget _idBuilder(AccountProvider account) {
+    var days = account.account.age.inDays;
+    var lenPost = ["today_l", "yesterday_l", "days_ago"];
+    return SliverToBoxAdapter(
+      child: Column(
+        children: [
+          LoaderWidget(
+              AssetType.vector, "avatar_${account.account.user.avatarUrl}"),
+          SizedBox(height: 10.d),
+          Text(
+            account.account.user.displayName!.startsWith("u_")
+                ? "You"
+                : account.account.user.displayName!,
+            style: TStyles.large,
+          ),
+          Text(
+            "${"join_at".l()} ${days > 1 ? days : ''} ${lenPost[days.max(2)].l()}",
+            style: TStyles.smallDetails,
+          ),
+        ],
+      ),
+    );
+  }
     );
   }
 }
